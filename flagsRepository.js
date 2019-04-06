615358182f876e5172904164a7f17a9d3dcb5c87
PhilQuand <PhilQuand@gmail.com>
2019-04-06 19:30:32 +0200

    Insertion initiale drapeaux

--- flagsRepository.js
+++ flagsRepository.js
@@ -1,62 +1,62 @@
 var flagsRepository = {
     Afghanistan : 
-    { img : "https://1.bp.blogspot.com/-lm2r7zAuMzc/XJ_mJX4vrqI/AAAAAAAAijo/4lFYOVjhkW8Mb1dy2arTLOIJ6pMwqi2SwCLcBGAs/s1600/afghanistan-flag-small.png",
+    { img : "https://philquand.github.io/Javascript/flagsRepository/Afghanistan.png",
       lat: 35.84 ,
       lng: 66.34 },
     AfriqueDuSud : 
-    { img : "https://2.bp.blogspot.com/-QnNQ_o1TrCY/XJ-m2DblIOI/AAAAAAAAii8/yWKOliDe6rka6wwjVf0YXxHo02wqx4W_wCLcBGAs/s1600/south-africa-flag-small.png",
+    { img : "https://philquand.github.io/Javascript/flagsRepository/AfriqueDuSud.png",
       lat: -27.44 ,
       lng: 24.55 },
     Algérie : 
-    { img : "https://3.bp.blogspot.com/-4mRPWEEPWqY/XJ_jbb2jvzI/AAAAAAAAijI/cYmQLhfEOTsLj4u4Yp2ZfCA6oY42tYBgQCLcBGAs/s1600/algeria-flag-small.png",
+    { img : "https://philquand.github.io/Javascript/flagsRepository/Algérie.png",
       lat: 25 ,
       lng: 3.32 },
     Albanie : 
-    { img : "https://lh3.googleusercontent.com/-IsUyHMQ3fKM/XHWf-kOknOI/AAAAAAAAibA/RAc5jHWs1fA60REqLKwT75cCB7vEY2p1ACHMYCw/s0/NewImage.png",
+    { img : "https://philquand.github.io/Javascript/flagsRepository/Albanie.png",
       lat: 41.25 ,
       lng: 20. },
     Allemagne   : 
-    { img :"https://lh3.googleusercontent.com/-8WFfRkVIR74/XAZoQMQUSOI/AAAAAAAAiAQ/NyupHgdsd1ImuYWPDc7rxEGTwmt_YmQzACHMYCw/s0/de.png",
+    { img :"https://philquand.github.io/Javascript/flagsRepository/Allemagne.png",
       lat: 49. ,
       lng: 9.7 },
     Australie : 
-    { img :"https://lh3.googleusercontent.com/-Mk-VZu7qeC8/XHWhWJGRQWI/AAAAAAAAibk/_rTdgijEMgklI1rt0z_JNfzz6bU3TobCgCHMYCw/s0/NewImage.png",
+    { img :"https://philquand.github.io/Javascript/flagsRepository/Australie.png",
       lat: -26 ,
       lng: 121 },
     Belgique : 
-    { img :"https://lh3.googleusercontent.com/-ODUMSPuaqiM/XHWgTrv6Z1I/AAAAAAAAibI/K-WI7GJimuwXZxDS-GKPcpKHe1v7h6jOgCHMYCw/s0/NewImage.png",
+    { img :"https://philquand.github.io/Javascript/flagsRepository/Belgique.png",
       lat: 51.5 , 
       lng: 4.7 },
     Californie  : 
-    { img :'https://lh3.googleusercontent.com/-OxqeZQxAU0I/XED-3GpUxZI/AAAAAAAAiMc/AHij0V-hlkAjbo3OCgNj1dDjzMj3-pU8wCLcBGAs/h240/ca.png',
+    { img :'https://philquand.github.io/Javascript/flagsRepository/Californie.png',
       lat: 34.050133,
       lng: -118.256324 },
     Colombie : 
-    { img :"https://lh3.googleusercontent.com/-j05ulv_mzyU/XHWfmXflsRI/AAAAAAAAia4/Cyry-KoDSisFOxSszQEP2rFHQgwXz_L0wCHMYCw/s0/NewImage.png",
+    { img :"https://philquand.github.io/Javascript/flagsRepository/Colombie.png",
       lat: 4.5 ,
       lng: -74.4 },
     Congo : 
-    { img :"https://lh3.googleusercontent.com/-7IMPh3lDQnM/XHWef0e9B9I/AAAAAAAAiag/2TE1HsudbacbsRSWECVrvrvy4-aDdCUpQCHMYCw/s0/NewImage.png",
+    { img :"https://philquand.github.io/Javascript/flagsRepository/Congo.png",
      lat: 1,
      lng: 23.321095 },
     CoteIvoire : 
-    { img : "https://4.bp.blogspot.com/-ApvWTY8DRAE/XKHQ_3S1FxI/AAAAAAAAikI/c8m2DJr2A7Uy2CxrggZE3AeygALthuveQCLcBGAs/s1600/ci.png",
+    { img : "https://philquand.github.io/Javascript/flagsRepository/CoteIvoire.png",
      lat: 6.10 ,
      lng: -5.65 },
     Espagne     : 
-    { img :"https://lh3.googleusercontent.com/-1ly6LP3awfc/XBOscwTDN_I/AAAAAAAAiCU/XV8UjMUvGaINOjCuyE81Me_2-Nd7SfPWQCHMYCw/s0/es.png",
+    { img :"https://philquand.github.io/Javascript/flagsRepository/Espagne.png",
     lat: 38.6,
     lng: -5 },
     France      : 
-    { img :"https://lh3.googleusercontent.com/-3p9wIlYxoho/XAWXYvAZpoI/AAAAAAAAh_4/Eirfipvf8z0Shz92eq1AXOuVaoufvpYBwCHMYCw/s0/fr.png",
+    { img :"https://philquand.github.io/Javascript/flagsRepository/France.png",
       lat: 48.,
       lng: 2. },
     Guinée      : 
-    { img : "https://lh3.googleusercontent.com/-BmITsUmX3Mo/XEDuPw-iSNI/AAAAAAAAiME/a5swMuwWHUQtBSC3w9QGH6oOD4wCP8AnQCLcBGAs/h240/gn.png",
+    { img : "https://philquand.github.io/Javascript/flagsRepository/Guinée.png",
     lat: 9.638600,
     lng: -13.575542 },
     Grèce      : 
-    { img : "https://3.bp.blogspot.com/-PuUlJy7mdKQ/XJ_mxnStHMI/AAAAAAAAijw/iS2pd78cvzMVykBake_g_RL5j_miFu0LQCLcBGAs/s1600/greece-flag-small.png",
+    { img : "https://philquand.github.io/Javascript/flagsRepository/Grèce.png",
     lat: 39.94,
     lng: 21.33 },
     Illinois      : 
@@ -64,27 +64,27 @@ var flagsRepository = {
     lat: 42.73,
     lng: -87.4 },
     Italie      : 
-    { img :"https://lh3.googleusercontent.com/-6L9Af6AHaG8/XAZoW2QAFzI/AAAAAAAAiAU/dLMRF4mKrekbNjE6056PeGL0-kOMjIcNQCHMYCw/s0/it.png",
+    { img :"https://philquand.github.io/Javascript/flagsRepository/Italie.png",
     lat: 41.3,
     lng: 15 },
     Kenya      : 
-    { img : "https://1.bp.blogspot.com/-BHmsR0ATGO8/XJ_kQQb7TbI/AAAAAAAAijU/vb918uMJA6UIWrl5e7IZPr2Lfrel7xNlgCLcBGAs/s1600/kenya-flag-small.png",
+    { img : "https://philquand.github.io/Javascript/flagsRepository/Kenya.png",
     lat: -2,
     lng: 38 },
     Mali      : 
-    { img : "https://2.bp.blogspot.com/-4Z51yCDHqeA/XKItzDmfUUI/AAAAAAAAil0/D4rqfGCa8kEpsh8nKtZwUZWLVoD3PVcdwCLcBGAs/s1600/ml.png",
+    { img : "https://philquand.github.io/Javascript/flagsRepository/Mali.png",
     lat: 20.57,
     lng: -3.07 },
     Maroc      : 
-    { img : "https://1.bp.blogspot.com/-SgLbbe2_RYY/XJ_lcQK5fqI/AAAAAAAAijg/v3LRgcoyRiQvg-tD_zNDAOZTSqSxZhf1ACLcBGAs/s1600/morocco-flag-small.png",
+    { img : "https://philquand.github.io/Javascript/flagsRepository/Maroc.png",
     lat: 33.48,
     lng: -4.91 },
     Netherlands : 
-    { img :"https://lh3.googleusercontent.com/-AeU0j6GXTTY/XHWgz5oTrPI/AAAAAAAAibU/FR3HVGFzyagW2NTABgaqlJ6WRT3-XB_VACHMYCw/s0/NewImage.png",
+    { img :"https://philquand.github.io/Javascript/flagsRepository/Netherlands.png",
       lat: 53 ,
       lng: 6.2 },
     Niger : 
-    { img :"https://lh3.googleusercontent.com/-quAY5_v1iCg/XHWdiIB7f-I/AAAAAAAAiaY/YJtcHLVVI0MwL1aVaICHpM9j_Jeqb1KzACHMYCw/s0/NewImage.png",
+    { img :"https://philquand.github.io/Javascript/flagsRepository/Niger.png",
       lat: 19,
       lng: 10 },
     Ontario    : 
@@ -92,35 +92,35 @@ var flagsRepository = {
     lat: 53.23,
     lng: -88.09 },
     Ouganda    : 
-    { img : "https://3.bp.blogspot.com/-rmvGTwpi5vg/XKIsPNMtUpI/AAAAAAAAilo/8CalubnPBiMvZxeXp4UUeHIVtwfqGihJwCLcBGAs/s1600/ug.png",
+    { img : "https://philquand.github.io/Javascript/flagsRepository/Ouganda.png",
     lat: 2.56,
     lng: 32.59 },
     Portugal    : 
-    { img :"https://lh3.googleusercontent.com/--T7VcUSBID8/XAZoeAUM-iI/AAAAAAAAiAY/AQk1y5JDcqU9-EAl5zEKo9ypp3CKvxa4ACHMYCw/s0/pt.png",
+    { img :"https://philquand.github.io/Javascript/flagsRepository/Portugal.png",
     lat: 41,
     lng: -8.2 },
     Québec      : 
-    { img :'https://lh3.googleusercontent.com/-w4z_8-Ehak4/XEA9zqaSgQI/AAAAAAAAiLU/TRXU87BFGJEwdwUEYnHdlM0Pu0jCmAf1QCLcBGAs/h240/langfr-450px-Flag_of_Quebec.svg.png',
+    { img :'https://philquand.github.io/Javascript/flagsRepository/Québec.png',
       lat: 46.829656,
       lng: -71.207210 },
     RoyaumesUnis: 
-    { img :"https://lh3.googleusercontent.com/-LGhG4UUw5qE/XAWQIIa_Z9I/AAAAAAAAh_s/R2x7iNfnufwIQiScynjY8y7bNPPGvEMOwCHMYCw/s0/en.png",
+    { img :"https://philquand.github.io/Javascript/flagsRepository/RoyaumesUnis.png",
       lat: 52,
       lng: -1 },
     Sénégal     : 
-    { img :"https://lh3.googleusercontent.com/-nG02UvQv-yg/XED0NOvLhuI/AAAAAAAAiMQ/JX-BowVk22UmYr42A-prmOXY3DqHGqo5QCLcBGAs/h240/sn.png",
+    { img :"https://philquand.github.io/Javascript/flagsRepository/Sénégal.png",
       lat: 16.032327,
       lng: -16.480443 },
     Suisse : 
-    { img :"https://lh3.googleusercontent.com/-PXKLvWBS744/XHWhF8Oh0cI/AAAAAAAAibc/1kShodOgXho9V7zkgQroHtWkLNTtHyP5QCHMYCw/s0/NewImage.png",
+    { img :"https://philquand.github.io/Javascript/flagsRepository/Suisse.png",
       lat: 46.25 ,
       lng: 7.5 },
     Tunisie : 
-    { img :"https://lh3.googleusercontent.com/-Dh8MaC8NbJY/XHWfCxOyFXI/AAAAAAAAias/9qgevZsXwaACXHG7wJ7KD1ElfNRrGSbvwCHMYCw/s0/NewImage.png",
+    { img :"https://philquand.github.io/Javascript/flagsRepository/Tunisie.png",
       lat: 35.5 ,
       lng: 9.511568 },
     USA         : 
-    { img :'https://lh3.googleusercontent.com/-8pZFsbitTCU/XH6tD64gckI/AAAAAAAAicw/DGJWdKPtNOYDbBpUZf_Yj7tMv7pS9jM9ACHMYCw/s0/united-states-of-america-flag-button-square-icon-64.png',
+    { img :'https://philquand.github.io/Javascript/flagsRepository/USA.png',
       lat: 34.050133,
       lng: -118.256324 },
     Zambie    : 

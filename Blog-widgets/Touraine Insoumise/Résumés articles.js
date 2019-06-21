55d07a865c0a8995a42427195ae7a9f7e6f4be0b
PhilQuand <PhilQuand@gmail.com>
2019-06-21 12:51:46 +0200

    Mise à jour widgets

--- Blog-widgets/Touraine Insoumise/Résumés articles.js
+++ Blog-widgets/Touraine Insoumise/Résumés articles.js
@@ -1,3 +1,12 @@
+<!-- FancyBox CSS -->
+<!-- see documentation at : http://fancyapps.com/fancybox/3/docs/#options -->
+<link href="http://cdnjs.cloudflare.com/ajax/libs/fancybox/3.1.25/jquery.fancybox.min.css" rel="stylesheet" type="text/css" />
+
+<!-- jQuery for FancyBox 3-->
+<script src='//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+
+<!-- FancyBox 3 -->
+<script src='https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.1.25/jquery.fancybox.min.js'></script>
 <style>
 .mySumFeat, .mySumReg {
 line-height: 1.6;

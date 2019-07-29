f97d94b59403447d14f9be8d34a6252c4ece1d1d
PhilQuand <PhilQuand@gmail.com>
2019-07-29 12:22:21 +0200

    Update myFancybox.js

--- PhotoGaleries/myFancybox/myFancybox.js
+++ PhotoGaleries/myFancybox/myFancybox.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $(".post-body").find("a img").each(function() {
+  $("#page_body").find("a img").each(function() {
       var isImage = /\.(?:jpg|jpeg|gif|png)$/i.test($(this).parent("a").attr("href"));
 
       if (isImage) {
@@ -40,18 +40,18 @@ $(document).ready(function() {
     animationEffect: "fade",
     transitionEffect: "slide",
     speed: 300,
-    margin: [40, 20],               
-    buttons : false,
-    afterLoad : function( instance, current ) {
-    if ( instance.group.length > 1 && current.$content ) {
-      current.$content.append('<a data-fancybox-next class="button-next" href="javascript:;">→</a><a data-fancybox-prev class="button-previous" href="javascript:;">←</a>');
+    margin: [40, 20],
+    buttons: false,
+    afterLoad: function(instance, current) {
+      if (instance.group.length > 1 && current.$content) {
+        current.$content.append('<a data-fancybox-next class="button-next" href="javascript:;">→</a><a data-fancybox-prev class="button-previous" href="javascript:;">←</a>');
+      }
+      /*if ( instance.group.length > 1 && current.$content ) {
+        current.$content.append('<a data-fancybox-next class="button-next" href="javascript:;">→</a><a data-fancybox-previous class="button-previous" href="javascript:;">←</a>');
+      }*/
+
+      //current.$content.append('<a data-fancybox-close class="button-close" href="javascript:;">×</a>');
     }
-    /*if ( instance.group.length > 1 && current.$content ) {
-      current.$content.append('<a data-fancybox-next class="button-next" href="javascript:;">→</a><a data-fancybox-previous class="button-previous" href="javascript:;">←</a>');
-    }*/
-    
-    //current.$content.append('<a data-fancybox-close class="button-close" href="javascript:;">×</a>');
-  }
 
   });
 });

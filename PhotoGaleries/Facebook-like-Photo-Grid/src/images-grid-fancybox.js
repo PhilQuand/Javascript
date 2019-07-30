cfa771b536120b0d416f36351916c94a2419c703
PhilQuand <PhilQuand@gmail.com>
2019-07-30 09:30:17 +0200

    Update images-grid-fancybox.js

--- PhotoGaleries/Facebook-like-Photo-Grid/src/images-grid-fancybox.js
+++ PhotoGaleries/Facebook-like-Photo-Grid/src/images-grid-fancybox.js
@@ -97,7 +97,7 @@
 
         if( this.opts.fancybox && this.opts.element.find('div').first().length > 0 ) {
           var element = this.opts.element.find("div");
-          var lastPicture = this.$element.find('.imgs-grid-image:last .image-wrap');
+          var lastPicture = element.find('.imgs-grid-image:last .image-wrap');
           lastPicture.find('<span>').click(function(){ 
             this.onImageClick.bind(this) }
           )

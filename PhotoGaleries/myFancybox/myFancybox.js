3269eb2811b7f4981b182e5524fa5c8892922fc1
PhilQuand <PhilQuand@gmail.com>
2019-08-02 16:12:32 +0200

    Update myFancybox.js

--- PhotoGaleries/myFancybox/myFancybox.js
+++ PhotoGaleries/myFancybox/myFancybox.js
@@ -50,6 +50,7 @@ $(document).ready(function() {
     buttons : [
       'thumbs'
     ],
+    baseClass : 'customInlineBaseCass',
     afterLoad: function(instance, current) {
       if (instance.group.length > 1 && current.$content) {
         if( current.index == 0 ) current.$content.append('<a data-fancybox-next class="button-next" href="javascript:;">→</a>');

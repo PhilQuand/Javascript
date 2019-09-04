55fe03d3effd77a9a62149d8a18f3de9c9fca6ac
PhilQuand <PhilQuand@gmail.com>
2019-09-04 11:03:36 +0200

    Update jquery-social-share-bar.js

--- FloatingShareBar/js/jquery-social-share-bar.js
+++ FloatingShareBar/js/jquery-social-share-bar.js
@@ -105,7 +105,10 @@
 
           $(document).ready(function() {
             $(".trgCalendarFancy").fancybox({
-                baseClass: 'calendarFancyClass'
+                baseClass: 'calendarFancyClass',
+                afterLoad: function(instance, current) {
+                    current.$content.append('<a data-fancybox-close class="button-close" href="javascript:;">×</a>');
+                }
             });
 
             $('.tooltipCalendar').tooltipster({

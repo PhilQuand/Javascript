5555d8c64a22a988084b70b980df1d3b99a1235c
PhilQuand <PhilQuand@gmail.com>
2019-09-25 09:40:40 +0200

    Update carte-infos-semaines.js

--- Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/carte-infos-semaines.js
+++ Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/carte-infos-semaines.js
@@ -20,12 +20,12 @@
     }
     if (iFrameGalleryOpt.archi.length > 0) iFrameGalleryOpt.archi[0].idGlinkID = 'MapLetters';
     iFrameGalleryOpt.accessmode = 'linkMapID';
-    var dateDeb = href[hrefLength - 1].dateDeb;
-    var strDeb = href[hrefLength - 1].dateDeb.getFullYear() + "-" + ("0" + (href[hrefLength - 1].dateDeb.getMonth() + 1)).slice(-2) + "-" + ("0" + href[hrefLength - 1].dateDeb.getDate()).slice(-2);
-    var hrefAfDeb = hrefLength - 4;
-    if( hrefAfDeb < 0) hrefAfDeb = 0;
-    var dateFin = href[hrefAfDeb].dateFin;
-    var strFin = href[hrefAfDeb].dateFin.getFullYear() + "-" + ("0" + (href[hrefAfDeb].dateFin.getMonth() + 1)).slice(-2) + "-" + ("0" + href[hrefAfDeb].dateFin.getDate()).slice(-2);
+    var hrefAfDeb = 3;
+    if( hrefLength < 4) hrefAfDeb = hrefLength - 1;
+    var dateDeb = href[hrefAfDeb].dateDeb;
+    var strDeb = href[hrefAfDeb].dateDeb.getFullYear() + "-" + ("0" + (hrefAfDeb].dateDeb.getMonth() + 1)).slice(-2) + "-" + ("0" + href[hrefAfDeb].dateDeb.getDate()).slice(-2);
+    var dateFin = href[0].dateFin;
+    var strFin = href[0].dateFin.getFullYear() + "-" + ("0" + (href[0].dateFin.getMonth() + 1)).slice(-2) + "-" + ("0" + href[0].dateFin.getDate()).slice(-2);
     var infoMapWrap = $('<div id="infoMap-wrapper"><div class="DateRange-wrapper"><span>' + strMapTitle + '</span><label for="from"> du : </label><input type="text" class="datepick" id="from" name="from" value="' + strDeb + '"> <label for="to"> au : </label><input type="text" class="datepick" id="to" name="to" value="' + strFin + '"> <button class="getFancyFocus  ui-button ui-widget ui-corner-all">OK</button></div></div>');
     var infoRows = $(this).find('.infoEvents-wrapper');
     if (infoRows.length == 0) {
@@ -51,11 +51,10 @@
       []
     ];
     var indexCal = 0;
-    var indexEnd = href.length - hrefAfDeb;
+    var indexEnd = hrefAfDeb;
     var indIndex = [];
-    for(i=0; i<=indexEnd; i++) {
-      var myindIndex = i+hrefAfDeb;
-      indIndex.push(myindIndex);
+    for(i=0; i<=hrefAfDeb; i++) {
+      indIndex.push(i);
     }
     var nbMapEvents = 0;
     var divLegend;

a599d68d55852dfbb53c751f3a3003aa26a7524b
PhilQuand <PhilQuand@gmail.com>
2019-10-27 15:36:17 +0100

    Update iFrameGallery-Plugin.js

--- PhotoGaleries/myFancybox/iFrameGallery-Plugin.js
+++ PhotoGaleries/myFancybox/iFrameGallery-Plugin.js
@@ -22,6 +22,8 @@
       var divArchi = $('<div id="divArchi" style="display: flex; flex-wrap: wrap;"></div>');
       $(this).append(divArchi);
 
+      var parentIFrameSize = {};
+      var iFrameNum = {};
       for (i = 0; i < options.archi.length; i++) {
 
         var imgID = options.archi[i].imgID;
@@ -33,6 +35,13 @@
         var pdfLink = options.archi[i].pdfLink;
         var inlineProgram = options.archi[i].inlineProgram;
         if (typeof inlineProgram === 'undefined') inlineProgram = 'inlineProgram';
+        if (typeof iFrameNum[inlineProgram] === 'undefined') iFrameNum[inlineProgram] = 0;
+        var inlineProgramIndex = inlineProgram + '-' + ++iFrameNum[inlineProgram];
+        if (typeof options.parentIFrameSize !== 'undefined') parentIFrameSize[inlineProgramIndex] = parseInt(options.parentIFrameSize);
+        if (typeof options.archi[i].parentIFrameSize !== 'undefined') {
+          parentIFrameSize[inlineProgramIndex] = parseInt(options.archi[i].parentIFrameSize);
+        }
+        if (iFrameNum[inlineProgram] == 1) parentIFrameSize[inlineProgram] = parentIFrameSize[inlineProgramIndex];
 
         if (idG.split("id=").length > 1) {
           idG = idG.split("id=")[1];
@@ -191,26 +200,30 @@
         baseClass: 'iFrameGalleryClass',
 
         afterClose: function(instance, current) {
-if ('parentIFrame' in window) {
-parentIFrame.scrollTo(0,0);
-}
+          if ('parentIFrame' in window) {
+            parentIFrame.scrollTo(0, 0);
+          }
         },
         beforeClose: function(instance, current) {
-if ('parentIFrame' in window) {
-parentIFrame.autoResize(true);
-}
+          if ('parentIFrame' in window) {
+            parentIFrame.autoResize(true);
+          }
         },
         afterShow: function(instance, current) {
-          var  fancybox_content = $(".iFrameGalleryClass .fancybox-content");
+          var fancybox_content = $(".iFrameGalleryClass .fancybox-content");
           fancybox_content.prop("style", "");
         },
         afterLoad: function(instance, current) {
-if ('parentIFrame' in window) {
-parentIFrame.autoResize(false);
-  parentIFrame.size(1000); // Set height to 400px
-//parentIFrame.scrollToOffset(0,0);
-parentIFrame.scrollTo(0,0);
-}
+          if ('parentIFrame' in window) {
+            if (parentIFrameSize[instance.currentHash] > 0) {
+              parentIFrame.autoResize(false);
+              parentIFrame.size(parentIFrameSize[instance.currentHash]); // Set height to parentIFrameSize
+            }
+            else parentIFrame.size();
+            //parentIFrame.scrollToOffset(0,0);
+            parentIFrame.scrollTo(0, 0);
+          }
+          //console.info( instance.Thumbs );
           //console.info( instance.Thumbs.$button[0]  );
           $("button.fancybox-button.fancybox-button--thumbs").click(function() {
             var elements = $(".iFrameGalleryClass .fancybox-thumbs__list").find("a").filter(function() {
@@ -221,12 +234,12 @@ parentIFrame.scrollTo(0,0);
             elements.css("display", "none");
           });
           if (instance.group.length > 1 && current.$content) {
-            var  arrow_left = $(".iFrameGalleryClass .fancybox-button.fancybox-button--arrow_left");
-            arrow_left.css("display","block");
-            var  arrow_right = $(".iFrameGalleryClass .fancybox-button.fancybox-button--arrow_right");
-            arrow_right.css("display","block");
-            if (current.index == 0) arrow_left.css("display","none");
-            else if (current.index == (instance.group.length - 1)) arrow_right.css("display","none");
+            var arrow_left = $(".iFrameGalleryClass .fancybox-button.fancybox-button--arrow_left");
+            arrow_left.css("display", "block");
+            var arrow_right = $(".iFrameGalleryClass .fancybox-button.fancybox-button--arrow_right");
+            arrow_right.css("display", "block");
+            if (current.index == 0) arrow_left.css("display", "none");
+            else if (current.index == (instance.group.length - 1)) arrow_right.css("display", "none");
           }
           /*if ( instance.group.length > 1 && current.$content ) {
             if (current.index == 0) current.$content.append('<a data-fancybox-next class="button-next" href="javascript:;">→</a>');
@@ -253,7 +266,7 @@ parentIFrame.scrollTo(0,0);
  * @license MIT
  */
 /*jslint bitwise: true */
-(function ($) {
+(function($) {
   'use strict';
 
   var root = typeof window === 'object' ? window : {};
@@ -265,31 +278,91 @@ parentIFrame.scrollTo(0,0);
   var AMD = typeof define === 'function' && define.amd;
   var BASE64_ENCODE_CHAR = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'.split('');
   var BASE64_DECODE_CHAR = {
-    'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8,
-    'J': 9, 'K': 10, 'L': 11, 'M': 12, 'N': 13, 'O': 14, 'P': 15, 'Q': 16, 
-    'R': 17, 'S': 18, 'T': 19, 'U': 20, 'V': 21, 'W': 22, 'X': 23, 'Y': 24, 
-    'Z': 25, 'a': 26, 'b': 27, 'c': 28, 'd': 29, 'e': 30, 'f': 31, 'g': 32, 
-    'h': 33, 'i': 34, 'j': 35, 'k': 36, 'l': 37, 'm': 38, 'n': 39, 'o': 40, 
-    'p': 41, 'q': 42, 'r': 43, 's': 44, 't': 45, 'u': 46, 'v': 47, 'w': 48, 
-    'x': 49, 'y': 50, 'z': 51, '0': 52, '1': 53, '2': 54, '3': 55, '4': 56, 
-    '5': 57, '6': 58, '7': 59, '8': 60, '9': 61, '+': 62, '/': 63, '-': 62,
+    'A': 0,
+    'B': 1,
+    'C': 2,
+    'D': 3,
+    'E': 4,
+    'F': 5,
+    'G': 6,
+    'H': 7,
+    'I': 8,
+    'J': 9,
+    'K': 10,
+    'L': 11,
+    'M': 12,
+    'N': 13,
+    'O': 14,
+    'P': 15,
+    'Q': 16,
+    'R': 17,
+    'S': 18,
+    'T': 19,
+    'U': 20,
+    'V': 21,
+    'W': 22,
+    'X': 23,
+    'Y': 24,
+    'Z': 25,
+    'a': 26,
+    'b': 27,
+    'c': 28,
+    'd': 29,
+    'e': 30,
+    'f': 31,
+    'g': 32,
+    'h': 33,
+    'i': 34,
+    'j': 35,
+    'k': 36,
+    'l': 37,
+    'm': 38,
+    'n': 39,
+    'o': 40,
+    'p': 41,
+    'q': 42,
+    'r': 43,
+    's': 44,
+    't': 45,
+    'u': 46,
+    'v': 47,
+    'w': 48,
+    'x': 49,
+    'y': 50,
+    'z': 51,
+    '0': 52,
+    '1': 53,
+    '2': 54,
+    '3': 55,
+    '4': 56,
+    '5': 57,
+    '6': 58,
+    '7': 59,
+    '8': 60,
+    '9': 61,
+    '+': 62,
+    '/': 63,
+    '-': 62,
     '_': 63
   };
 
-  var utf8ToBytes = function (str) {
+  var utf8ToBytes = function(str) {
     var bytes = [];
     for (var i = 0; i < str.length; i++) {
       var c = str.charCodeAt(i);
       if (c < 0x80) {
         bytes[bytes.length] = c;
-      } else if (c < 0x800) {
+      }
+      else if (c < 0x800) {
         bytes[bytes.length] = 0xc0 | (c >> 6);
         bytes[bytes.length] = 0x80 | (c & 0x3f);
-      } else if (c < 0xd800 || c >= 0xe000) {
+      }
+      else if (c < 0xd800 || c >= 0xe000) {
         bytes[bytes.length] = 0xe0 | (c >> 12);
         bytes[bytes.length] = 0x80 | ((c >> 6) & 0x3f);
         bytes[bytes.length] = 0x80 | (c & 0x3f);
-      } else {
+      }
+      else {
         c = 0x10000 + (((c & 0x3ff) << 10) | (str.charCodeAt(++i) & 0x3ff));
         bytes[bytes.length] = 0xf0 | (c >> 18);
         bytes[bytes.length] = 0x80 | ((c >> 12) & 0x3f);
@@ -300,11 +373,14 @@ parentIFrame.scrollTo(0,0);
     return bytes;
   };
 
-  var decodeAsBytes = function (base64Str) {
-    var v1, v2, v3, v4, bytes = [], index = 0, length = base64Str.length;
+  var decodeAsBytes = function(base64Str) {
+    var v1, v2, v3, v4, bytes = [],
+      index = 0,
+      length = base64Str.length;
     if (base64Str.charAt(length - 2) === '=') {
       length -= 2;
-    } else if (base64Str.charAt(length - 1) === '=') {
+    }
+    else if (base64Str.charAt(length - 1) === '=') {
       length -= 1;
     }
 
@@ -325,7 +401,8 @@ parentIFrame.scrollTo(0,0);
       v1 = BASE64_DECODE_CHAR[base64Str.charAt(i++)];
       v2 = BASE64_DECODE_CHAR[base64Str.charAt(i++)];
       bytes[index++] = (v1 << 2 | v2 >>> 4) & 255;
-    } else if (remain === 3) {
+    }
+    else if (remain === 3) {
       v1 = BASE64_DECODE_CHAR[base64Str.charAt(i++)];
       v2 = BASE64_DECODE_CHAR[base64Str.charAt(i++)];
       v3 = BASE64_DECODE_CHAR[base64Str.charAt(i++)];
@@ -335,8 +412,9 @@ parentIFrame.scrollTo(0,0);
     return bytes;
   };
 
-  var encodeFromBytes = function (bytes) {
-    var v1, v2, v3, base64Str = '', length = bytes.length;
+  var encodeFromBytes = function(bytes) {
+    var v1, v2, v3, base64Str = '',
+      length = bytes.length;
     for (var i = 0, count = parseInt(length / 3) * 3; i < count;) {
       v1 = bytes[i++];
       v2 = bytes[i++];
@@ -346,7 +424,7 @@ parentIFrame.scrollTo(0,0);
         BASE64_ENCODE_CHAR[(v2 << 2 | v3 >>> 6) & 63] +
         BASE64_ENCODE_CHAR[v3 & 63];
     }
-    
+
     // remain char
     var remain = length - count;
     if (remain === 1) {
@@ -354,7 +432,8 @@ parentIFrame.scrollTo(0,0);
       base64Str += BASE64_ENCODE_CHAR[v1 >>> 2] +
         BASE64_ENCODE_CHAR[(v1 << 4) & 63] +
         '==';
-    } else if (remain === 2) {
+    }
+    else if (remain === 2) {
       v1 = bytes[i++];
       v2 = bytes[i];
       base64Str += BASE64_ENCODE_CHAR[v1 >>> 2] +
@@ -365,29 +444,33 @@ parentIFrame.scrollTo(0,0);
     return base64Str;
   };
 
-  var btoa = root.btoa, atob = root.atob, utf8Base64Encode, utf8Base64Decode;
+  var btoa = root.btoa,
+    atob = root.atob,
+    utf8Base64Encode, utf8Base64Decode;
   if (NODE_JS) {
     var Buffer = require('buffer').Buffer;
-    btoa = function (str) {
+    btoa = function(str) {
       return new Buffer(str, 'ascii').toString('base64');
     };
 
-    utf8Base64Encode = function (str) {
+    utf8Base64Encode = function(str) {
       return new Buffer(str).toString('base64');
     };
 
     encodeFromBytes = utf8Base64Encode;
 
-    atob = function (base64Str) {
+    atob = function(base64Str) {
       return new Buffer(base64Str, 'base64').toString('ascii');
     };
 
-    utf8Base64Decode = function (base64Str) {
+    utf8Base64Decode = function(base64Str) {
       return new Buffer(base64Str, 'base64').toString();
     };
-  } else if (!btoa) {
-    btoa = function (str) {
-      var v1, v2, v3, base64Str = '', length = str.length;
+  }
+  else if (!btoa) {
+    btoa = function(str) {
+      var v1, v2, v3, base64Str = '',
+        length = str.length;
       for (var i = 0, count = parseInt(length / 3) * 3; i < count;) {
         v1 = str.charCodeAt(i++);
         v2 = str.charCodeAt(i++);
@@ -397,7 +480,7 @@ parentIFrame.scrollTo(0,0);
           BASE64_ENCODE_CHAR[(v2 << 2 | v3 >>> 6) & 63] +
           BASE64_ENCODE_CHAR[v3 & 63];
       }
-      
+
       // remain char
       var remain = length - count;
       if (remain === 1) {
@@ -405,7 +488,8 @@ parentIFrame.scrollTo(0,0);
         base64Str += BASE64_ENCODE_CHAR[v1 >>> 2] +
           BASE64_ENCODE_CHAR[(v1 << 4) & 63] +
           '==';
-      } else if (remain === 2) {
+      }
+      else if (remain === 2) {
         v1 = str.charCodeAt(i++);
         v2 = str.charCodeAt(i);
         base64Str += BASE64_ENCODE_CHAR[v1 >>> 2] +
@@ -416,8 +500,10 @@ parentIFrame.scrollTo(0,0);
       return base64Str;
     };
 
-    utf8Base64Encode = function (str) {
-      var v1, v2, v3, base64Str = '', bytes = utf8ToBytes(str), length = bytes.length;
+    utf8Base64Encode = function(str) {
+      var v1, v2, v3, base64Str = '',
+        bytes = utf8ToBytes(str),
+        length = bytes.length;
       for (var i = 0, count = parseInt(length / 3) * 3; i < count;) {
         v1 = bytes[i++];
         v2 = bytes[i++];
@@ -427,7 +513,7 @@ parentIFrame.scrollTo(0,0);
           BASE64_ENCODE_CHAR[(v2 << 2 | v3 >>> 6) & 63] +
           BASE64_ENCODE_CHAR[v3 & 63];
       }
-      
+
       // remain char
       var remain = length - count;
       if (remain === 1) {
@@ -435,7 +521,8 @@ parentIFrame.scrollTo(0,0);
         base64Str += BASE64_ENCODE_CHAR[v1 >>> 2] +
           BASE64_ENCODE_CHAR[(v1 << 4) & 63] +
           '==';
-      } else if (remain === 2) {
+      }
+      else if (remain === 2) {
         v1 = bytes[i++];
         v2 = bytes[i];
         base64Str += BASE64_ENCODE_CHAR[v1 >>> 2] +
@@ -446,11 +533,13 @@ parentIFrame.scrollTo(0,0);
       return base64Str;
     };
 
-    atob = function (base64Str) {
-      var v1, v2, v3, v4, str = '', length = base64Str.length;
+    atob = function(base64Str) {
+      var v1, v2, v3, v4, str = '',
+        length = base64Str.length;
       if (base64Str.charAt(length - 2) === '=') {
         length -= 2;
-      } else if (base64Str.charAt(length - 1) === '=') {
+      }
+      else if (base64Str.charAt(length - 1) === '=') {
         length -= 1;
       }
 
@@ -471,34 +560,43 @@ parentIFrame.scrollTo(0,0);
         v1 = BASE64_DECODE_CHAR[base64Str.charAt(i++)];
         v2 = BASE64_DECODE_CHAR[base64Str.charAt(i++)];
         str += String.fromCharCode((v1 << 2 | v2 >>> 4) & 255);
-      } else if (remain === 3) {
+      }
+      else if (remain === 3) {
         v1 = BASE64_DECODE_CHAR[base64Str.charAt(i++)];
         v2 = BASE64_DECODE_CHAR[base64Str.charAt(i++)];
         v3 = BASE64_DECODE_CHAR[base64Str.charAt(i++)];
         str += String.fromCharCode((v1 << 2 | v2 >>> 4) & 255) +
-               String.fromCharCode((v2 << 4 | v3 >>> 2) & 255);
+          String.fromCharCode((v2 << 4 | v3 >>> 2) & 255);
       }
       return str;
     };
 
-    utf8Base64Decode = function (base64Str) {
-      var str = '', bytes = decodeAsBytes(base64Str), length = bytes.length;
-      var i = 0, followingChars = 0, b, c;
+    utf8Base64Decode = function(base64Str) {
+      var str = '',
+        bytes = decodeAsBytes(base64Str),
+        length = bytes.length;
+      var i = 0,
+        followingChars = 0,
+        b, c;
       while (i < length) {
         b = bytes[i++];
         if (b <= 0x7F) {
           str += String.fromCharCode(b);
           continue;
-        } else if (b > 0xBF && b <= 0xDF) {
+        }
+        else if (b > 0xBF && b <= 0xDF) {
           c = b & 0x1F;
           followingChars = 1;
-        } else if (b <= 0xEF) {
+        }
+        else if (b <= 0xEF) {
           c = b & 0x0F;
           followingChars = 2;
-        } else if (b <= 0xF7) {
+        }
+        else if (b <= 0xF7) {
           c = b & 0x07;
           followingChars = 3;
-        } else {
+        }
+        else {
           throw 'not a UTF-8 string';
         }
 
@@ -519,7 +617,8 @@ parentIFrame.scrollTo(0,0);
 
         if (c <= 0xFFFF) {
           str += String.fromCharCode(c);
-        } else {
+        }
+        else {
           c -= 0x10000;
           str += String.fromCharCode((c >> 10) + 0xD800);
           str += String.fromCharCode((c & 0x3FF) + 0xDC00);
@@ -527,21 +626,25 @@ parentIFrame.scrollTo(0,0);
       }
       return str;
     };
-  } else {
-    utf8Base64Encode = function (str) {
+  }
+  else {
+    utf8Base64Encode = function(str) {
       var result = '';
       for (var i = 0; i < str.length; i++) {
         var charcode = str.charCodeAt(i);
         if (charcode < 0x80) {
           result += String.fromCharCode(charcode);
-        } else if (charcode < 0x800) {
+        }
+        else if (charcode < 0x800) {
           result += String.fromCharCode(0xc0 | (charcode >> 6)) +
             String.fromCharCode(0x80 | (charcode & 0x3f));
-        } else if (charcode < 0xd800 || charcode >= 0xe000) {
+        }
+        else if (charcode < 0xd800 || charcode >= 0xe000) {
           result += String.fromCharCode(0xe0 | (charcode >> 12)) +
             String.fromCharCode(0x80 | ((charcode >> 6) & 0x3f)) +
             String.fromCharCode(0x80 | (charcode & 0x3f));
-        } else {
+        }
+        else {
           charcode = 0x10000 + (((charcode & 0x3ff) << 10) | (str.charCodeAt(++i) & 0x3ff));
           result += String.fromCharCode(0xf0 | (charcode >> 18)) +
             String.fromCharCode(0x80 | ((charcode >> 12) & 0x3f)) +
@@ -552,27 +655,35 @@ parentIFrame.scrollTo(0,0);
       return btoa(result);
     };
 
-    utf8Base64Decode = function (base64Str) {
+    utf8Base64Decode = function(base64Str) {
       var tmpStr = atob(base64Str.trim('=').replace(/-/g, '+').replace(/_/g, '/'));
       if (!/[^\x00-\x7F]/.test(tmpStr)) {
         return tmpStr;
       }
-      var str = '', i = 0, length = tmpStr.length, followingChars = 0, b, c;
+      var str = '',
+        i = 0,
+        length = tmpStr.length,
+        followingChars = 0,
+        b, c;
       while (i < length) {
         b = tmpStr.charCodeAt(i++);
         if (b <= 0x7F) {
           str += String.fromCharCode(b);
           continue;
-        } else if (b > 0xBF && b <= 0xDF) {
+        }
+        else if (b > 0xBF && b <= 0xDF) {
           c = b & 0x1F;
           followingChars = 1;
-        } else if (b <= 0xEF) {
+        }
+        else if (b <= 0xEF) {
           c = b & 0x0F;
           followingChars = 2;
-        } else if (b <= 0xF7) {
+        }
+        else if (b <= 0xF7) {
           c = b & 0x07;
           followingChars = 3;
-        } else {
+        }
+        else {
           throw 'not a UTF-8 string';
         }
 
@@ -585,7 +696,7 @@ parentIFrame.scrollTo(0,0);
           c += b & 0x3F;
         }
         if (c >= 0xD800 && c <= 0xDFFF) {
-          throw 'not a UTF-8 string'; 
+          throw 'not a UTF-8 string';
         }
         if (c > 0x10FFFF) {
           throw 'not a UTF-8 string';
@@ -593,7 +704,8 @@ parentIFrame.scrollTo(0,0);
 
         if (c <= 0xFFFF) {
           str += String.fromCharCode(c);
-        } else {
+        }
+        else {
           c -= 0x10000;
           str += String.fromCharCode((c >> 10) + 0xD800);
           str += String.fromCharCode((c & 0x3FF) + 0xDC00);
@@ -602,25 +714,27 @@ parentIFrame.scrollTo(0,0);
       return str;
     };
   }
-  
-  var encode = function (str, asciiOnly) {
+
+  var encode = function(str, asciiOnly) {
     var notString = typeof(str) != 'string';
     if (notString && str.constructor === root.ArrayBuffer) {
       str = new Uint8Array(str);
     }
     if (notString) {
       return encodeFromBytes(str);
-    } else {
+    }
+    else {
       if (!asciiOnly && /[^\x00-\x7F]/.test(str)) {
         return utf8Base64Encode(str);
-      } else {
+      }
+      else {
         return btoa(str);
       }
     }
   };
 
   jQuery.fn.extend({
-    svgtobase64: function () {
+    svgtobase64: function() {
       var svgInfosstr = $(this).get(0).outerHTML;
       var svgInfosbase64r = base64.encode(svgInfosstr);
       var imgBase64 = 'data:image/svg+xml;base64,' + svgInfosbase64r;
@@ -628,7 +742,7 @@ parentIFrame.scrollTo(0,0);
     }
   });
 
-  var decode = function (base64Str, asciiOnly) {
+  var decode = function(base64Str, asciiOnly) {
     return asciiOnly ? atob(base64Str) : utf8Base64Decode(base64Str);
   };
 
@@ -643,9 +757,10 @@ parentIFrame.scrollTo(0,0);
 
   if (COMMON_JS) {
     module.exports = exports;
-  } else {
+  }
+  else {
     root.base64 = exports;
-    if(AMD) {
+    if (AMD) {
       define(function() {
         return exports;
       });

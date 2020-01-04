a420356523a2fb1491d7e755cb51277b1c90c386
PhilQuand <PhilQuand@gmail.com>
2020-01-04 19:17:57 +0100

    CN Lettres 2020 initialisation

---
+++ Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Année 2020/carte-infos-semaines-Archives.js
@@ -0,0 +1,135 @@
+    var archi = [
+         {
+          svgInfos: ['Semaine 52', 'du 23 dec.', 'au 27 dec.', '2019'],
+          idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 52.html',
+          idGlinkID: 'PreviousLetters'
+        },
+       {
+          svgInfos: ['Semaine 51', 'du 16 dec.', 'au 20 dec.', '2019'],
+          idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 51.html',
+        },
+        {
+          svgInfos: ['Semaine 50', 'du 09 dec.', 'au 13 dec.', '2019'],
+          idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 50.html',
+        },
+        {
+          svgInfos: ['Semaine 49', 'du 02 dec.', 'au 06 dec.', '2019'],
+          idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 49.html',
+        },
+        {
+          svgInfos: ['Semaine 48', 'du 25 nov.', 'au 29 nov.', '2019'],
+          idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 48.html',
+        },
+        {
+          svgInfos: ['Semaine 47', 'du 18 nov.', 'au 22 nov.', '2019'],
+          idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 47.html',
+        },
+        {
+          svgInfos: ['Semaine 46', 'du 11 nov.', 'au 15 nov.', '2019'],
+          idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 46.html',
+        },
+        {
+          svgInfos: ['Semaine 45', 'du 04 nov.', 'au 08 nov.', '2019'],
+          idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 45.html',
+        },
+        {
+          svgInfos: ['Semaine 44', 'du 28 oct.', 'au 01 nov.', '2019'],
+          idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 44.html',
+        },
+         {
+        svgInfos: ['Semaine 43', 'du 21 oct.', 'au 25 oct.', '2019'],
+        idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 43.html',
+       },
+        {
+        svgInfos: ['Semaine 42', 'du 14 oct.', 'au 18 oct.', '2019'],
+        idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 42.html',
+       },
+         {
+        svgInfos: ['Semaine 41', 'du 07 oct.', 'au 11 oct.', '2019'],
+        idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 41.html',
+       },
+         {
+        svgInfos: ['Semaine 40', 'du 30 sept.', 'au 04 oct.', '2019'],
+        idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 40.html',
+       },
+         {
+        svgInfos: ['Semaine 39', 'du 23 sept.', 'au 27 sept.', '2019'],
+        idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 39.html',
+       },
+         {
+        svgInfos: ['Semaine 38', 'du 16 sept.', 'au 20 sept.', '2019'],
+        idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 38.html',
+       },
+         {
+        svgInfos: ['Semaine 37', 'du 9 sept.', 'au 13 sept.', '2019'],
+        idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 37.html'
+       },
+        {
+        svgInfos: ['Semaine 36', 'du 2 sept.', 'au 6 sept.', '2019'],
+        idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 36.html'
+      },
+       {
+        imgID: 'svgBlue',
+        svgInfos: ["Moi d'août", '2019'],
+        svgInfosCarte: ['Semaine 35', 'du 26 août', 'au 30 août', '2019'],
+        idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 35.html'
+      },
+        {
+        svgInfosCarte: ['Semaine 34', 'du 19 août', 'au 25 août', '2019'],
+        idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 34.html'
+      },
+      {
+        svgInfosCarte: ['Semaine 33', 'du 12 août', 'au 18 août', '2019'],
+        idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 33.html'
+      },
+      {
+        svgInfosCarte: ['Semaine 32', 'du 5 août', 'au 9 août', '2019'],
+        idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 32.html'
+      },
+     {
+        imgID: 'svgBlue',
+        svgInfos: ['Moi de juillet', '2019'],
+        svgInfosCarte: ['Semaine 31', 'du 29 juillet', 'au 2 août', '2019'],
+        idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 31.html'
+      },
+      {
+        svgInfosCarte: ['Semaine 30', 'du 22 juillet', 'au 26 juillet', '2019'],
+        idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 30.html'
+      },
+      {
+        svgInfosCarte: ['Semaine 29', 'du 15 juillet', 'au 19 juillet', '2019'],
+        idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 29.html'
+      },
+       {
+        svgInfosCarte: ['Semaine 28', 'du 8 juillet', 'au 12 juillet', '2019'],
+        idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 28.html'
+      },
+       {
+        svgInfosCarte: ['Semaine 27', 'du 1 juillet', 'au 5 juillet', '2019'],
+        idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 27.html'
+      },
+       {
+        imgID: 'svgBlue',
+        svgInfos: ['Moi de juin', '2019'],
+        svgInfosCarte: ['Semaine 26', 'du 24 juin', 'au 28 juin', '2019'],
+        idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 26.html'
+      },
+      {
+        svgInfosCarte: ['Semaine 25', 'du 17 juin', 'au 21 juin', '2019'],
+        idG: 'https://docs.google.com/gview?url=https://coordination-defense-sante.org/wp-content/uploads/2019/06/Infos-semaine-25-au-17-au-21-Juin-2019-1.pdf&embedded=true',
+      },
+       {
+        svgInfosCarte: ['Semaine 24', 'du 11 juin', 'au 14 juin', '2019'],
+        idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 24.html'
+      },
+       {
+        svgInfosCarte: ['Semaine 23', 'du 03 juin', 'au 07 juin', '2019'],
+        idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 23 bis.html'
+      },
+      {
+        imgID: 'svgGreen',
+        svgInfos: ['Autres'],
+        idG: '#autre'/*,
+        idGExt: 'http://coordination-defense-sante.org/bulletin-hebdomadaire/'*/
+      }
+    ]
---
+++ Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Année 2020/carte-infos-semaines-Icons.js
@@ -0,0 +1,16 @@
+var simpleGreenIcon = new L.Icon({
+  iconUrl: 'http://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+  shadowUrl: 'http://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+var simpleBlueIcon = new L.Icon({
+  iconUrl: 'http://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+  shadowUrl: 'http://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});

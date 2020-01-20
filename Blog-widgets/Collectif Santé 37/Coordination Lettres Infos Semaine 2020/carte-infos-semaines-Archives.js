3ba54fa965aaca79cacb09239378061ca9d47671
PhilQuand <PhilQuand@gmail.com>
2020-01-20 14:17:33 +0100

    CN Infos semaine 03 2020

--- Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2020/carte-infos-semaines-Archives.js
+++ Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2020/carte-infos-semaines-Archives.js
@@ -1,8 +1,12 @@
     var archi = [
          {
+          svgInfos: ['Semaine 03', 'du 13 jan.', 'au 17 jan.', '2020'],
+          idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2020/Semaine 03.html',
+          idGlinkID: 'PreviousLetters'
+        },
+         {
           svgInfos: ['Semaine 02', 'du 06 jan.', 'au 10 jan.', '2020'],
           idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2020/Semaine 02.html',
-          idGlinkID: 'PreviousLetters'
         },
          {
           svgInfos: ['Semaine 01', 'du 30 dec.', 'au 03 jan.', '2020'],

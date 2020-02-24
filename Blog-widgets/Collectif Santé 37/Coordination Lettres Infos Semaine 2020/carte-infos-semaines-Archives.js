f87ae67d93a4c0ea897c0d9799a27f1526d9cf1b
PhilQuand <PhilQuand@gmail.com>
2020-02-24 14:41:46 +0100

    Update carte-infos-semaines-Archives.js

--- Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2020/carte-infos-semaines-Archives.js
+++ Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2020/carte-infos-semaines-Archives.js
@@ -1,8 +1,12 @@
     var archi = [
-           {
+          {
+          svgInfos: ['Semaine 08', 'du 17 feb.', 'au 21 feb.', '2020'],
+          idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2020/Semaine 08.html',
+          idGlinkID: 'PreviousLetters'
+        },
+          {
           svgInfos: ['Semaine 07', 'du 10 feb.', 'au 14 feb.', '2020'],
           idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2020/Semaine 07.html',
-          idGlinkID: 'PreviousLetters'
         },
          {
           svgInfos: ['Semaine 06', 'du 03 feb.', 'au 07 feb.', '2020'],
@@ -28,7 +32,7 @@
           svgInfos: ['Semaine 01', 'du 30 dec.', 'au 03 jan.', '2020'],
           idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2020/Semaine 01.html',
         },
-         {
+         /*{
           svgInfos: ['Semaine 52', 'du 23 dec.', 'au 27 dec.', '2019'],
           idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 52.html',
         },
@@ -43,5 +47,5 @@
         {
           svgInfos: ['Semaine 49', 'du 02 dec.', 'au 06 dec.', '2019'],
           idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine/Semaine 49.html',
-        },
+        },*/
     ]

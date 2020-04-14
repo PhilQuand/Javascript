24d38e8c3df051a7c6bd74028c86f2310813b755
PhilQuand <philippe.quandalle@yahoo.fr>
2020-04-14 17:47:19 +0200

    Update carte-infos-semaines-Archives.js

--- Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2020/carte-infos-semaines-Archives.js
+++ Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2020/carte-infos-semaines-Archives.js
@@ -1,8 +1,12 @@
     var archi = [
            {
+          svgInfos: ['Semaine 15', 'du 06 apr.', 'au 10 apr.', '2020'],
+          idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2020/Semaine 15.html',
+          idGlinkID: 'PreviousLetters'
+        },
+           {
           svgInfos: ['Semaine 14', 'du 30 mar.', 'au 03 apr.', '2020'],
           idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2020/Semaine 14.html',
-          idGlinkID: 'PreviousLetters'
         },
            {
           svgInfos: ['Semaine 13', 'du 23 mar.', 'au 27 mar.', '2020'],

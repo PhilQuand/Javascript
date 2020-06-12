637c89b844cff9d9647866e058c4ef96cbf7ae88
PhilQuand <philippe.quandalle@yahoo.fr>
2020-06-12 12:48:43 +0200

    Update CollectifSante37_widget.js

--- Blog-widgets/Collectif Santé 37/CollectifSante37_widget.js
+++ Blog-widgets/Collectif Santé 37/CollectifSante37_widget.js
@@ -586,13 +586,13 @@ function createSummaryAndThumb(pID, isRegular) {
 <a style="display:none" class="fancybox fancybox.iframe" data-type="iframe" data-fancybox="iframes" data-src="https://geodes.santepubliquefrance.fr/index.php#bbox=-162564,6318414,679313,535880&c=indicator&f=0&i=covid_hospit.rad&t=a01&view=map2" href="javascript:;" >iframe 4</a>
 <a style="display:none" class="fancybox fancybox.iframe" data-type="iframe" data-fancybox="iframes" data-src="https://thevirustracker.com/" href="javascript:;" >iframe 5</a-->
 
-<div class="articleFancy" id="hidden-content-1">
+<!--div class="articleFancy" id="hidden-content-1">
 <div class="corpsFancy">
-<img src="https://1.bp.blogspot.com/-DHejtkKvDGg/XtjwQiJibeI/AAAAAAAAmVE/H_9vACECauUG7BX1PiqtvUOeL5uv-UeogCK4BGAsYHg/s320/tract%2Bunitaire%2B16%2Bjuin%2B2020%2BV%2B3.jpg" />
+<img src="https://1.bp.blogspot.com/-FJDjMPOenx4/XuIfoidV9BI/AAAAAAAAmYI/tSp1eLB5URw75B9UAHIkpUkeCAJGxwiZQCK4BGAsYHg/s1600/Capture%2Bd%25E2%2580%2599e%25CC%2581cran%2B2020-06-11%2Ba%25CC%2580%2B14.11.22.jpg" />
 </div>
 </div>
 <a style="display:none" class="trgImgUL" data-src="#hidden-content-1" data-fancybox="images" data-width="1132" data-height="1600">TEST Fancy</a>
-<!--div class="articleFancy" id="hidden-content-2">
+<div class="articleFancy" id="hidden-content-2">
 <div class="corpsFancy">
 <img src="https://1.bp.blogspot.com/-VKgaKWKbOq8/Xkq4tDRM-kI/AAAAAAAAlBc/aDkpX-FLoEoRpOXMi_ScH5QIjNNrWUicgCPcBGAYYCw/s1600/Capture%2Bd%25E2%2580%2599e%25CC%2581cran%2B2020-02-17%2Ba%25CC%2580%2B16.59.53.png" />
 </div>
@@ -609,34 +609,28 @@ function createSummaryAndThumb(pID, isRegular) {
 <img src="https://1.bp.blogspot.com/-r4UZygiIxfo/Xkq4rkiwgUI/AAAAAAAAlBY/IBOahb50pnUBYAM37xxp3VO3WO6tpsRGACPcBGAYYCw/s1600/Capture%2Bd%25E2%2580%2599e%25CC%2581cran%2B2020-02-17%2Ba%25CC%2580%2B16.59.39.png" />
 </div>
 </div>
-<a style="display:none" class="trgImgUL" data-src="#hidden-content-4" data-fancybox="images" data-width="1132" data-height="1600">TEST Fancy</a>--
-<!--a style="display:none" class="trgImgUL" href="https://1.bp.blogspot.com/-m66JTk-a1oY/Xd9_VShqHFI/AAAAAAAAki8/YgooSCCzwAEPv236ldJ8Qj-4ES3HezXBwCK4BGAYYCw/s1600/Appel-5-Dec.jpeg" data-fancybox="images" data-width="679" data-height="960">TEST Fancy</a-->
+<a style="display:none" class="trgImgUL" data-src="#hidden-content-4" data-fancybox="images" data-width="1132" data-height="1600">TEST Fancy</a>
+<a style="display:none" class="trgImgUL" href="https://1.bp.blogspot.com/-m66JTk-a1oY/Xd9_VShqHFI/AAAAAAAAki8/YgooSCCzwAEPv236ldJ8Qj-4ES3HezXBwCK4BGAYYCw/s1600/Appel-5-Dec.jpeg" data-fancybox="images" data-width="679" data-height="960">TEST Fancy</a-->
 
-<!--div class="articleFancy" id="hidden-content-1">
+<div class="articleFancy" id="hidden-content-1">
 <div class="corpsFancy">
 <div style="font-size:24px; text-align: center; line-height: 150%; margin-top: 2em">
-Quatre organisations syndicales du département appellent à 
-<br/>
-<b>une mobilisation le 27 mai</b>, 
-<br/>(date anniversaire du Conseil Nationale de la Résistance)
-<br/>autour du champ Giraud, siège de L'Agence Régionale de Santé, 
-<br/>pour réclamer des moyens 
-<br/>pour l'hôpital public, les EHPAD, 
-<br>pour la protection sociale, 
-<br/>et pour que la crise sanitaire ne devienne pas une crise sociale qu'on nous fera payer
-<p>Le Collectif santé appelle à participer à cette mobilisation, en respectant bien sûr, les mesures barrières</p>
-<p style="color:red"><b>Rendez-vous le 27 mai à 18 H devant le Champs Giraud</b></p>
+Le 16 juin sera une journée historique de mobilisation dans  la santé<br/>
+après l’épidémie du coronavirus au début de laquelle <br/>
+les personnels dans les hôpitaux, les EHPAD et les services à domicile ont pris soin des malades et des personnes dépendantes sans protection.
+
+<p style="color:red"><b>Rendez-vous le mardi 16 juin à 15H place Jean Jaurès</b></p>
 </div>
 </div>
 </div>
 <a style="display:none" class="trgImgUL" data-src="#hidden-content-1" data-fancybox="images" data-width="1132" data-height="1600">TEST Fancy</a>
 <div class="articleFancy" id="hidden-content-2">
 <div class="corpsFancy">
-<img src="https://1.bp.blogspot.com/-aanYftpD7HQ/XskFSWS5VJI/AAAAAAAAmEU/Pac4kMDJyCArC5YIQJAzTwh08FgQfekPwCK4BGAsYHg/s1600/image-1.png" />
+<img src="https://1.bp.blogspot.com/-FJDjMPOenx4/XuIfoidV9BI/AAAAAAAAmYI/tSp1eLB5URw75B9UAHIkpUkeCAJGxwiZQCK4BGAsYHg/s1600/Capture%2Bd%25E2%2580%2599e%25CC%2581cran%2B2020-06-11%2Ba%25CC%2580%2B14.11.22.jpg" />
 </div>
 </div>
 <a style="display:none" class="trgImgUL" data-src="#hidden-content-2" data-fancybox="images" data-width="1132" data-height="1600">TEST Fancy</a>
-<div class="articleFancy" id="hidden-content-3">
+<!--div class="articleFancy" id="hidden-content-3">
 <div class="corpsFancy">
 <img src="https://1.bp.blogspot.com/-G3PSWiGvqf8/XskUGfVQM-I/AAAAAAAAmEw/s2upFEbjY5YgNsq4k1xUJvkk9fcMI8tBgCK4BGAsYHg/s1600/98306570_1286226648246897_920476663366549504_n.jpg" />
 </div>
@@ -805,7 +799,7 @@ if ( window.location.pathname == '/' ){
   //$('.page_body').append('<a href="https://collectifsante37.blogspot.com/#iframes-1"><img class="img-foreground" src="https://1.bp.blogspot.com/-DYdZzA-CRZU/XoHet8_fCWI/AAAAAAAAlRg/R4T6v1XagJ0ckuzhD8MDG6H6IQddEdDrwCLcBGAsYHQ/s1600/coronavirus_generic.png" /></a>');
   //$('.page_body').append('<a href="https://collectifsante37.blogspot.com/#iframes-1"><div class="img-foreground gallery-container"></div></a>');
   //$('.page_body').append('<a href="https://collectifsante37.blogspot.com/#images-1"><img class="img-foreground" src="https://1.bp.blogspot.com/-q2VTR14q1qk/Xsj-_RM40jI/AAAAAAAAmD4/fgtSYY6FmVMETIbHuAsUYchU696jBXofACK4BGAsYHg/s1600/Azo%2B2020%2B03%2B01%2BMe%25CC%2581gaphone%252Bpetit.png" /></a>');
-  $('.page_body').append('<a href="https://collectifsante37.blogspot.com/#images"><img class="img-foreground" src="https://1.bp.blogspot.com/-DHejtkKvDGg/XtjwQiJibeI/AAAAAAAAmVE/H_9vACECauUG7BX1PiqtvUOeL5uv-UeogCK4BGAsYHg/s320/tract%2Bunitaire%2B16%2Bjuin%2B2020%2BV%2B3.jpg" /></a>');
+  $('.page_body').append('<a href="https://collectifsante37.blogspot.com/#images-1"><img class="img-foreground" src="https://1.bp.blogspot.com/-FJDjMPOenx4/XuIfoidV9BI/AAAAAAAAmYI/tSp1eLB5URw75B9UAHIkpUkeCAJGxwiZQCK4BGAsYHg/s1600/Capture%2Bd%25E2%2580%2599e%25CC%2581cran%2B2020-06-11%2Ba%25CC%2580%2B14.11.22.jpg" /></a>');
 }
 </script>
 <style>

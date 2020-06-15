64f3024e88f7ff516ba66207251036929fc75171
PhilQuand <philippe.quandalle@yahoo.fr>
2020-06-15 18:11:29 +0200

    Update Archives.js

--- Blog-widgets/Collectif Santé 37/Vus-Ailleurs/Vus-Ailleurs-2020/Archives.js
+++ Blog-widgets/Collectif Santé 37/Vus-Ailleurs/Vus-Ailleurs-2020/Archives.js
@@ -1,12 +1,16 @@
     var archi = [
          {
+          svgInfos: ['Semaine 24', 'du 8 juin.', 'au 14 juin', '2020'],
+          idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Vus-Ailleurs/Vus-Ailleurs-2020/Semaine-2020-24.html',
+        },
+         {
           svgInfos: ['Semaine 23', 'du 01 juin.', 'au 7 juin', '2020'],
           idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Vus-Ailleurs/Vus-Ailleurs-2020/Semaine-2020-23.html',
+          idGlinkID: 'PreviousLetters'
         },
          {
           svgInfos: ['Semaine 22', 'du 25 may.', 'au 31 may', '2020'],
           idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Vus-Ailleurs/Vus-Ailleurs-2020/Semaine-2020-22.html',
-          idGlinkID: 'PreviousLetters'
         },
        {
           svgInfos: ['Semaine 21', 'du 18 may.', 'au 24 may', '2020'],

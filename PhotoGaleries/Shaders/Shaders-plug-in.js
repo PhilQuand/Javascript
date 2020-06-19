9f9b09c7f979c3b2fbe8d0f1ccd56cb1e6ccd34e
PhilQuand <philippe.quandalle@yahoo.fr>
2020-06-19 12:25:25 +0200

    Update Shaders-plug-in.js

--- PhotoGaleries/Shaders/Shaders-plug-in.js
+++ PhotoGaleries/Shaders/Shaders-plug-in.js
@@ -1,9 +1,5 @@
 import ShadersGallery from 'https://philquand.github.io/Javascript/PhotoGaleries/Shaders/Shaders17.js';
 (function($) {
-  var script_arr = [
-    'https://cdnjs.cloudflare.com/ajax/libs/animejs/3.1.0/anime.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/three.js/110/three.min.js'
-  ];
   $.fn.Shaders = function(options) {
     if (jQuery.type(options) != 'undefined') {
       if (jQuery.type(options.urls) == 'undefined' ||
@@ -46,8 +42,15 @@ import ShadersGallery from 'https://philquand.github.io/Javascript/PhotoGaleries
           }
         };
         //$.getScript('https://cdnjs.cloudflare.com/ajax/libs/animejs/3.1.0/anime.min.js', function() {
-        $.getMultiScripts(script_arr, '').done(function() {
+        //$.getMultiScripts(script_arr,'').done(function() {
+        Promise.all([
+          load.js('https://cdnjs.cloudflare.com/ajax/libs/animejs/3.1.0/anime.min.js'),
+          load.js('https://cdnjs.cloudflare.com/ajax/libs/three.js/110/three.min.js'),
+        ]).then(function() {
+          console.log('Shaders: Everything has loaded!');
           window.myGallery[randNum] = new ShadersGallery(ShadersGalleryoptions);
+        }).catch(function() {
+          console.log('Shaders: Oh no, epic failure!');
         });
         console.log(window.myGallery[randNum]);
         $(this).wrapInner('<a href="' + window.location.href + '#' + randNum + '-1"></a>');
@@ -68,15 +71,62 @@ import ShadersGallery from 'https://philquand.github.io/Javascript/PhotoGaleries
   Generator.prototype.getrand = function() {
     return this.rand++;
   };
-  $.getMultiScripts = function(arr, path) {
-    var _arr = $.map(arr, function(scr) {
-      return $.getScript((path || "") + scr);
-    });
+  /*var script_arr = [
+      'https://cdnjs.cloudflare.com/ajax/libs/animejs/3.1.0/anime.min.js', 
+      'https://cdnjs.cloudflare.com/ajax/libs/three.js/110/three.min.js'
+    ];
+    $.getMultiScripts = function(arr, path) {
+      var _arr = $.map(arr, function(scr) {
+          return $.getScript( (path||"") + scr );
+      });
 
-    _arr.push($.Deferred(function(deferred) {
-      $(deferred.resolve);
-    }));
+      _arr.push($.Deferred(function( deferred ){
+          $( deferred.resolve );
+      }));
 
-    return $.when.apply($, _arr);
-  }
+      return $.when.apply($, _arr);
+  }*/
+  var load = (function() {
+    // Function which returns a function: https://davidwalsh.name/javascript-functions
+    function _load(tag) {
+      return function(url) {
+        // This promise will be used by Promise.all to determine success or failure
+        return new Promise(function(resolve, reject) {
+          var element = document.createElement(tag);
+          var parent = 'body';
+          var attr = 'src';
+
+          // Important success and error for the promise
+          element.onload = function() {
+            resolve(url);
+          };
+          element.onerror = function() {
+            reject(url);
+          };
+
+          // Need to set different attributes depending on tag type
+          switch (tag) {
+            case 'script':
+              element.async = true;
+              break;
+            case 'link':
+              element.type = 'text/css';
+              element.rel = 'stylesheet';
+              attr = 'href';
+              parent = 'head';
+          }
+
+          // Inject into document to kick off loading
+          element[attr] = url;
+          document[parent].appendChild(element);
+        });
+      };
+    }
+
+    return {
+      css: _load('link'),
+      js: _load('script'),
+      img: _load('img')
+    }
+  })();
 })(jQuery);

ef1793f5ac9af5bb5b73fa225e78e74642cf0570
PhilQuand <philippe.quandalle@yahoo.fr>
2020-07-10 13:32:51 +0200

    Espoir Metabolique

--- Blog-widgets/EspoirMetabolique/Videos-EspoirMetaboliques.js
+++ Blog-widgets/EspoirMetabolique/Videos-EspoirMetaboliques.js
@@ -53,7 +53,7 @@ $('[data-fancybox-video]').fancybox({
       else if (current.index == (instance.group.length - 1)) current.$content.append('<a data-fancybox-prev class="button-previous" href="javascript:;">←</a>');
       else current.$content.append('<a data-fancybox-next class="button-next" href="javascript:;">→</a><a data-fancybox-prev class="button-previous" href="javascript:;">←</a>');
     }
-    current.$content.append('<a data-fancybox-close class="button-close" href="javascript:;">x</a>');
+    current.$content.append('<a data-fancybox-close class="button-close" href="javascript:;"><span  style="font-family: Arial">x</span></a>');
   },
   onUpdate: function(opts, obj) {
     var top = 0;

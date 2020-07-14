b87a0b162ec0af04690f2e7be23b4a86f9276a0f
PhilQuand <philippe.quandalle@yahoo.fr>
2020-07-14 16:52:20 +0200

    Update AlaUne.js

--- PhotoGaleries/myFancybox/AlaUne.js
+++ PhotoGaleries/myFancybox/AlaUne.js
@@ -188,7 +188,8 @@ import ShadersGallery from 'https://philquand.github.io/Javascript/PhotoGaleries
                   else if (current.index == (instance.group.length - 1)) current.$content.append('<a data-fancybox-prev class="button-previous" href="javascript:;">←</a>');
                   else current.$content.append('<a data-fancybox-next class="button-next" href="javascript:;">→</a><a data-fancybox-prev class="button-previous" href="javascript:;">←</a>');
                 }
-                current.$content.append('<a data-fancybox-close class="button-close" href="javascript:;"><span  style="font-family: Arial">x</span></a>');
+                if(current.$content.find('.button-close').length ==  0)
+                current.$content.prepend('<a data-fancybox-close class="button-close" href="javascript:;"><span  style="font-family: Arial">x</span></a>');
               },
               onUpdate: function(opts, obj) {
 

6959234b9168dfd1f8c5fa2c460ff8c5b10ce720
PhilQuand <philippe.quandalle@yahoo.fr>
2020-07-20 12:17:31 +0200

    Update AlaUne.js

--- PhotoGaleries/myFancybox/AlaUne.js
+++ PhotoGaleries/myFancybox/AlaUne.js
@@ -150,6 +150,7 @@ import ShadersGallery from 'https://philquand.github.io/Javascript/PhotoGaleries
                 baseClass: 'fancyAlaUneDefault fancyAlaUneDefaultButton',
                 smallBtn: false,
                 toolbar: false,
+	              type : 'iframe',
                 afterLoad: function(instance, current) {
 
                   var pixelRatio = window.devicePixelRatio || 1;

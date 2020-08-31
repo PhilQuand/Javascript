4cfa6898bcff5b509ce4f027524fb4ce326d90e1
PhilQuand <49251353+PhilQuand@users.noreply.github.com>
2020-08-31 14:55:07 +0200

    Update carte-infos-semaines-Archives.js

--- Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2020/carte-infos-semaines-Archives.js
+++ Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2020/carte-infos-semaines-Archives.js
@@ -1,12 +1,16 @@
     var archi = [
            {
+          svgInfos: ['Semaine 34-35', 'du 17 août.', 'au 30 août', '2020'],
+          idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2020/Semaine 34-35.html',
+        },
+           {
           svgInfos: ['Semaine 32-33', 'du 03 août.', 'au 15 août', '2020'],
           idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2020/Semaine 32-33.html',
+          idGlinkID: 'PreviousLetters'
         },
            {
           svgInfos: ['Semaine 31', 'du 27 juillet.', 'au 31 juillet', '2020'],
           idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2020/Semaine 31.html',
-          idGlinkID: 'PreviousLetters'
         },
            {
           svgInfos: ['Semaine 30', 'du 20 juillet.', 'au 24 juillet', '2020'],

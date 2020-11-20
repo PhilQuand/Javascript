1e60720c3ccbf3fd4db38b5e10475ae9c839af82
PhilQuand <philippe.quandalle@yahoo.fr>
2020-11-20 17:09:33 +0100

    Update HTML Résumés articles.js

--- Blog-widgets/Antivol/HTML Résumés articles.js
+++ Blog-widgets/Antivol/HTML Résumés articles.js
@@ -441,6 +441,69 @@ $(document).ready(function() {
 }
 </style>
 <style>
+h1 { 
+  display: block;
+  color: #D52C1E;
+  font-size: 2em;
+  //font-size:x-large;
+  margin-top: 0.67em;
+  margin-bottom: 0.67em;
+  margin-left: 0;
+  margin-right: 0;
+  font-weight: bold;
+}
+h2 {
+  display: block;
+  color: black;
+  font-size: 1.5em;
+  margin-top: 0.83em;
+  margin-bottom: 0.83em;
+  margin-left: 0;
+  margin-right: 0;
+  font-weight: bold;
+}
+h3 { 
+  display: block;
+  color: black;
+  font-size: 1.17em;
+  margin-top: 1em;
+  margin-bottom: 1em;
+  margin-left: 0;
+  margin-right: 0;
+  font-weight: bold;
+}
+h4 { 
+  display: block;
+  color: black;
+  font-size: 1em;
+  margin-top: 1.33em;
+  margin-bottom: 1.33em;
+  margin-left: 0;
+  margin-right: 0;
+  font-weight: bold;
+}
+h5 { 
+  display: block;
+  color: black;
+  font-size: .83em;
+  margin-top: 1.67em;
+  margin-bottom: 1.67em;
+  margin-left: 0;
+  margin-right: 0;
+  font-weight: bold;
+}
+h6 { 
+  display: block;
+  color: black;
+  font-size: .67em;
+  margin-top: 2.33em;
+  margin-bottom: 2.33em;
+  margin-left: 0;
+  margin-right: 0;
+  font-weight: bold;
+}
+</style>
+<style>
 .mySumReg img {
   max-height: 50px;
 }

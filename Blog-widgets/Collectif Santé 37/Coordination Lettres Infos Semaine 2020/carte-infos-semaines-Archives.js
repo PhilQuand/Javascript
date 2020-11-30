ff3c8502f42098b6691434fa29b019dbdac6b4e8
PhilQuand <philippe.quandalle@yahoo.fr>
2020-11-30 18:56:25 +0100

    Update carte-infos-semaines-Archives.js

--- Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2020/carte-infos-semaines-Archives.js
+++ Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2020/carte-infos-semaines-Archives.js
@@ -1,7 +1,7 @@
     var archi = [
            {
           svgInfos: ['Semaine 48', 'du 23 novembre', 'au 27 novembre', '2020'],
-          idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2020/Semaine 47.html',
+          idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2020/Semaine 48.html',
         },
            {
           svgInfos: ['Semaine 47', 'du 16 novembre', 'au 21 novembre', '2020'],

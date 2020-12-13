5e99bc96a1e81677198dcc450f3b3860a5313061
PhilQuand <philippe.quandalle@yahoo.fr>
2020-12-13 19:25:15 +0100

    Update Résumés articles.js

--- Blog-widgets/CATDP/Résumés articles.js
+++ Blog-widgets/CATDP/Résumés articles.js
@@ -261,68 +261,6 @@ function loadCATDPTimeLine() {
   });
 }*/
 </script>
-<style>
-h1 { 
-  display: block;
-  color: black;
-  font-size: 2em;
-  margin-top: 0.67em;
-  margin-bottom: 0.67em;
-  margin-left: 0;
-  margin-right: 0;
-  font-weight: bold;
-}
-h2 {
-  display: block;
-  color: black;
-  font-size: 1.5em;
-  margin-top: 0.83em;
-  margin-bottom: 0.83em;
-  margin-left: 0;
-  margin-right: 0;
-  font-weight: bold;
-}
-h3 { 
-  display: block;
-  color: black;
-  font-size: 1.17em;
-  margin-top: 1em;
-  margin-bottom: 1em;
-  margin-left: 0;
-  margin-right: 0;
-  font-weight: bold;
-}
-h4 { 
-  display: block;
-  color: black;
-  font-size: 1em;
-  margin-top: 1.33em;
-  margin-bottom: 1.33em;
-  margin-left: 0;
-  margin-right: 0;
-  font-weight: bold;
-}
-h5 { 
-  display: block;
-  color: black;
-  font-size: .83em;
-  margin-top: 1.67em;
-  margin-bottom: 1.67em;
-  margin-left: 0;
-  margin-right: 0;
-  font-weight: bold;
-}
-h6 { 
-  display: block;
-  color: black;
-  font-size: .67em;
-  margin-top: 2.33em;
-  margin-bottom: 2.33em;
-  margin-left: 0;
-  margin-right: 0;
-  font-weight: bold;
-}
-</style>
 <!--style>
 /* pour création pdf */
 /*.fr-FR, .en-EN, .de-DE, .es-ES, .it-IT, .pt-PT {

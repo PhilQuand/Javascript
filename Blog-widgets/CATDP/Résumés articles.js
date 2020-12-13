920a6b921eec9d9102e0f0b9d2bafefa481d87af
PhilQuand <philippe.quandalle@yahoo.fr>
2020-12-13 18:06:11 +0100

    Update Résumés articles.js

--- Blog-widgets/CATDP/Résumés articles.js
+++ Blog-widgets/CATDP/Résumés articles.js
@@ -11,6 +11,7 @@
 <script src='https://philquand.github.io/Javascript/PhotoGaleries/myFancybox/myFancybox.js'></script>
 <script src='https://philquand.github.io/Javascript/PhotoGaleries/myFancybox/iFrameGallery-Plugin.js'></script>
 <!-- End FancyBox -->
+<link href="https://philquand.github.io/Javascript/Blog-widgets/All-Blogs-Style.css" rel="stylesheet" type="text/css" />
 
 
 <style>
@@ -321,89 +322,6 @@ h6 {
   margin-left: 0;
   margin-right: 0;
   font-weight: bold;
-}
-</style>
-<!-- alignement vignette et texte -->
-<style>
-.VignetteBR.flex {
-    display: flex; 
-    align-items: flex-start;
-    width: 100%; 
-}
-.VignetteBR.flex .divImg {
-    max-width: 200px; 
-    width: 30%; 
-    //margin-bottom: 1em;
-}
-.VignetteBR.flex.left .divImg {
-    margin-right: 1em;
-}
-.VignetteBR.flex.right {
-    flex-direction: row-reverse;
-}
-.VignetteBR.flex.right .divImg {
-    margin-left: 0.5em;
-}
-.VignetteBR.flex .divText, .VignetteBR.flex .divImg img {
-    width: 100%;
-}
-.VignetteBR.flex .divImg.relief img {
-    box-shadow: rgba(0, 0, 0, 0.8) 10px 5px 25px;
-}
-@media (max-width: 1100px) {
-    .VignetteBR.flex {
-        display: block; 
-    }
-    .VignetteBR.flex .divText, .VignetteBR.flex .divImg {
-        display: block;
-        width: 100%; 
-        max-width: 100%;
-        margin-bottom: 1em;
-    }
-    .VignetteBR.flex .divImg img {
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-        width: 80%; 
-    }
-}
-.VignetteBR.float {
-    //display: block; 
-}
-.VignetteBR.float .divImg {
-    max-width: 200px; 
-    width: 40%; 
-    margin-top: 0.5em;
-    //margin-bottom: 0.5em;
-}
-.VignetteBR.float.left .divImg {
-    margin-right: 1em;
-    float: left;
-}
-.VignetteBR.float.right .divImg {
-    margin-left: 1em;
-    float: right;
-}
-.VignetteBR.float .divText, .VignetteBR.float .divImg img {
-    width: 100%; 
-}
-.VignetteBR.float .divImg.relief img {
-    box-shadow: rgba(0, 0, 0, 0.8) 10px 5px 25px;
-}
-@media (max-width: 1100px) {
-    .VignetteBR.float .divText, .VignetteBR.float .divImg {
-        display: block;
-        width: 100%; 
-        max-width: 100%;
-        margin-bottom: 1em;
-    }
-    .VignetteBR.float .divImg img {
-        display: block;
-        //max-width: 200px; 
-        margin-left: auto;
-        margin-right: auto;
-        //width: 80%; 
-    }
 }
 </style>
 <!--style>
@@ -1085,31 +1003,6 @@ $(document).ready(function() {
 </div>
 <a style="display:none" class="trgImgUL" data-src="#hidden-content-3" data-fancybox="images" data-width="1132" data-height="1600">TEST Fancy</a>
 <!--a style="display:none" class="trgImgUL" href="https://1.bp.blogspot.com/-m66JTk-a1oY/Xd9_VShqHFI/AAAAAAAAki8/YgooSCCzwAEPv236ldJ8Qj-4ES3HezXBwCK4BGAYYCw/s1600/Appel-5-Dec.jpeg" data-fancybox="images" data-width="679" data-height="960">TEST Fancy</a-->
-<style>
-/* pour rendre les vidéo "responsive" (voir sbipsetlab...) */
-.video-wrapper {
-	width: 560px;
-	max-width: 100%;
-    margin: 2em auto 2em auto;
-}
-.video-container {
-	position: relative;
-	padding-bottom: 56.25%;
-	//padding-top: 30px;
-	height: 0;
-	overflow: hidden;
-}
-
-.video-container iframe,  
-.video-container object,  
-.video-container embed {
-	position: absolute;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100%;
-}
-</style>
 <style>
 .articleFancy {
     padding: 0 0 0 0;

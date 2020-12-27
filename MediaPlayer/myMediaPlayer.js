90d8adad2b4142ffc1503800453c968669ee9ba9
PhilQuand <philippe.quandalle@yahoo.fr>
2020-12-27 15:15:40 +0100

    Update myMediaPlayer.js

--- MediaPlayer/myMediaPlayer.js
+++ MediaPlayer/myMediaPlayer.js
@@ -28,6 +28,38 @@
     timeDir: 'ltr',
     volumeDir: 'ltr'
   };
+
+  function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+      elmnt = z[i];
+      /*search for elements with a certain atrribute:*/
+      file = elmnt.getAttribute("w3-include-html");
+      if (file) {
+        /* Make an HTTP request using the attribute value as the file name: */
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+          if (this.readyState == 4) {
+            if (this.status == 200) {
+              elmnt.innerHTML = this.responseText;
+            }
+            if (this.status == 404) {
+              elmnt.innerHTML = "Page not found.";
+            }
+            /* Remove the attribute, and call this function once more: */
+            elmnt.removeAttribute("w3-include-html");
+            includeHTML();
+          }
+        }
+        xhttp.open("GET", file, true);
+        xhttp.send();
+        /* Exit the function: */
+        return;
+      }
+    }
+  }
   var load = (function() {
     // Function which returns a function: https://davidwalsh.name/javascript-functions
     function _load(tag) {
@@ -82,50 +114,51 @@
       console.log('Everything has loaded!');
       $('body').append('<div w3-include-html="https://philquand.github.io/Javascript/MediaPlayer/myMediaPlayer.html"></div>');
       $(document).ready(function() {
+        includeHTML();
         /*$('.audioFancy').find('audio').each(function() {
-          const $target = this;
-          const player = new MediaPlayer(
-            $target, {
-              prefix: 'media',
-              lang: {
-                play: 'play',
-                pause: 'pause',
-                mute: 'mute',
-                unmute: 'unmute',
-                volume: 'volume',
-                currentTime: 'current time',
-                remainingTime: 'remaining time',
-                enterFullscreen: 'enter fullscreen',
-                leaveFullscreen: 'leave fullscreen',
-                download: 'download'
-              },
-              svgs: {
-                play: '#symbol-play',
-                pause: '#symbol-pause',
-                mute: '#symbol-mute',
-                unmute: '#symbol-unmute',
-                volume: '#symbol-volume',
-                currentTime: '#symbol-currentTime',
-                remainingTime: '#symbol-remainingTime',
-                enterFullscreen: '#symbol-enterFullscreen',
-                leaveFullscreen: '#symbol-leaveFullscreen',
-                download: '#symbol-download'
-              },
-              timeDir: 'ltr',
-              volumeDir: 'ltr'
-            }
-          );
+           const $target = this;
+           const player = new MediaPlayer(
+             $target, {
+               prefix: 'media',
+               lang: {
+                 play: 'play',
+                 pause: 'pause',
+                 mute: 'mute',
+                 unmute: 'unmute',
+                 volume: 'volume',
+                 currentTime: 'current time',
+                 remainingTime: 'remaining time',
+                 enterFullscreen: 'enter fullscreen',
+                 leaveFullscreen: 'leave fullscreen',
+                 download: 'download'
+               },
+               svgs: {
+                 play: '#symbol-play',
+                 pause: '#symbol-pause',
+                 mute: '#symbol-mute',
+                 unmute: '#symbol-unmute',
+                 volume: '#symbol-volume',
+                 currentTime: '#symbol-currentTime',
+                 remainingTime: '#symbol-remainingTime',
+                 enterFullscreen: '#symbol-enterFullscreen',
+                 leaveFullscreen: '#symbol-leaveFullscreen',
+                 download: '#symbol-download'
+               },
+               timeDir: 'ltr',
+               volumeDir: 'ltr'
+             }
+           );
 
-        });*/
+         });*/
         $('audio, video').each(function() {
           const player = new MediaPlayer(this, MediaPlayerConfig);
         });
 
-          /*var promise = $('.media-control.media-play').click(); 
-          if (promise) {
-              //Older browsers may not return a promise, according to the MDN website
-              promise.catch(function(error) { console.error(error); });
-          }*/
+        /*var promise = $('.media-control.media-play').click(); 
+        if (promise) {
+            //Older browsers may not return a promise, according to the MDN website
+            promise.catch(function(error) { console.error(error); });
+        }*/
 
       })
 

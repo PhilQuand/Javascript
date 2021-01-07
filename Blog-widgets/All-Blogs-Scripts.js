2f52e83fa9cd33b36534ec02e9c3c793b61382db
PhilQuand <philippe.quandalle@yahoo.fr>
2021-01-07 15:34:00 +0100

    Update All-Blogs-Scripts.js

--- Blog-widgets/All-Blogs-Scripts.js
+++ Blog-widgets/All-Blogs-Scripts.js
@@ -9,7 +9,7 @@ function compSumAndImg(strx, chop, imgtag) {
   if (strx.indexOf("<") != -1) {
     var s = strx.split("<");
     var news = [];
-    if( imgtag == '' ) {
+    if (imgtag == '') {
       news.push('<br/>');
       chop += 5;
     }
@@ -28,10 +28,10 @@ function compSumAndImg(strx, chop, imgtag) {
       var isdebDiv = debDiv(s[i]);
       if (isdebDiv) {
         var debToDelete = debDivToDelete(s[i]);
-        if((isToDelete != 0) || debToDelete) {
+        if ((isToDelete != 0) || debToDelete) {
           isToDelete++;
         }
-      } 
+      }
       if (s[i].indexOf(">") != -1) {
         var isendDiv = endDiv(s[i]);
         if ((isToDelete > 0) && isendDiv) {
@@ -39,17 +39,18 @@ function compSumAndImg(strx, chop, imgtag) {
         }
         if (!(isToDelete > 0)) {
           var curS = '';
-          if (isTagtoSum(s,i)) {
+          if (isTagtoSum(s, i)) {
             //if(chopCount < chopIn) chop += s[i].indexOf(">") + 1
             curS = '<' + s[i];
-            if(chopCount < chopIn && !isTagMore(s,i)) {
+            if (chopCount < chopIn && !isTagMore(s, i)) {
               chop += s[i].indexOf(">") + 2;
-            } else isFinished = true;
+            }
+            else isFinished = true;
           }
           else {
             curS = s[i].substring(s[i].indexOf(">") + 1, s[i].length);
           }
-          chopCount += (s[i].length - s[i].indexOf(">")-1);
+          chopCount += (s[i].length - s[i].indexOf(">") - 1);
           // insert two line breaks after authors
           if ((i > 1) && (s[i - 2] == '/span>') && (s[i - 1] == '/strong>') && (s[i].indexOf('/p>') == 0)) {
             news.push('<br/>');
@@ -59,81 +60,84 @@ function compSumAndImg(strx, chop, imgtag) {
         }
       }
       //if( isFinished ) i = s.length;
-      if( isFinished ) break;
+      if (isFinished) break;
     }
     strx = news.join("");
   }
 
   if (msg != '') alert('news.length : ' + news.length + '\ns.length : ' + s.length + '\nmsg : \n' + msg);
   chop = (chop < strx.length - 1) ? chop : strx.length - 1;
-  if(chop != strx.length - 1) while (strx.charAt(chop) != ' ') chop--;
+  if (chop != strx.length - 1)
+    while (strx.charAt(chop) != ' ') chop--;
   strx = strx.substring(0, chop);
   result.summary = strx + '...';
   return result;
 
-function isTagtoSum(s,i) {
-  //if(s[i][0] == 'a' || s[i].indexOf('/a>') > -1) {
-  if( s[i][0] == 'a' && s[i+1].indexOf('img') != 0 ) {
-      return true; 
-  } 
-  if( s[i].indexOf('/a>') > -1 && s[i-1].indexOf('img') != 0 ) {
-      return true; 
-  } 
-  if( s[i].indexOf('strong') > -1 ) {
-    return true;
-  } 
-  if( s[i].indexOf('em>') > -1 ) {
-    return true;
-  } 
-  return false;
-}
-function isTagMore(s,i) {
-  if( s[i].indexOf('name="more"') > -1 ) {
-    return true;
-  } 
-  return false;
-}
-function debDivToDelete(s) {
-  var myTag = s.substring(0, s.indexOf(">"));
-  if ((myTag.indexOf('div') > -1) && (myTag.indexOf('data-summary="no"') > -1)) {
-    return true;
+  function isTagtoSum(s, i) {
+    //if(s[i][0] == 'a' || s[i].indexOf('/a>') > -1) {
+    if (s[i][0] == 'a' && s[i + 1].indexOf('img') != 0) {
+      return true;
+    }
+    if (s[i].indexOf('/a>') > -1 && s[i - 1].indexOf('img') != 0) {
+      return true;
+    }
+    if (s[i].indexOf('strong') > -1) {
+      return true;
+    }
+    if (s[i].indexOf('em>') > -1) {
+      return true;
+    }
+    return false;
   }
-  if ( (myTag.indexOf('script') == 0) ) {
-    return true;
-  }
-  if ( myTag.indexOf('style') == 0 ) {
-    return true;
-  }
-  return false;
-}
 
-function debDiv(s) {
-  var myTag = s.substring(0, s.indexOf(">"));
-  if (myTag.indexOf('div') > -1 && !(myTag.indexOf('/') > -1)) {
-    return true;
+  function isTagMore(s, i) {
+    if (s[i].indexOf('name="more"') > -1) {
+      return true;
+    }
+    return false;
   }
-  if ( (myTag.indexOf('script') == 0) && !((myTag.indexOf('/script') > -1) || (myTag[myTag.length-1] == '/'))) {
-    return true;
-  }
-  if ( (myTag.indexOf('style') == 0) && !((myTag.indexOf('/style') > -1) || (myTag[myTag.length-1] == '/'))) {
-    return true;
-  }
-  return false;
-}
 
-function endDiv(s) {
-  var myTag = s.substring(0, s.indexOf(">"));
-  if (myTag.indexOf('div') > -1 && myTag.indexOf('/') > -1) {
-    return true;
+  function debDivToDelete(s) {
+    var myTag = s.substring(0, s.indexOf(">"));
+    if ((myTag.indexOf('div') > -1) && (myTag.indexOf('data-summary="no"') > -1)) {
+      return true;
+    }
+    if ((myTag.indexOf('script') == 0)) {
+      return true;
+    }
+    if (myTag.indexOf('style') == 0) {
+      return true;
+    }
+    return false;
   }
-  if (myTag.indexOf('/script') == 0 ) {
-    return true;
+
+  function debDiv(s) {
+    var myTag = s.substring(0, s.indexOf(">"));
+    if (myTag.indexOf('div') > -1 && !(myTag.indexOf('/') > -1)) {
+      return true;
+    }
+    if ((myTag.indexOf('script') == 0) && !((myTag.indexOf('/script') > -1) || (myTag[myTag.length - 1] == '/'))) {
+      return true;
+    }
+    if ((myTag.indexOf('style') == 0) && !((myTag.indexOf('/style') > -1) || (myTag[myTag.length - 1] == '/'))) {
+      return true;
+    }
+    return false;
   }
-  if (myTag.indexOf('/style') == 0 ) {
-    return true;
+
+  function endDiv(s) {
+    var myTag = s.substring(0, s.indexOf(">"));
+    if (myTag.indexOf('div') > -1 && myTag.indexOf('/') > -1) {
+      return true;
+    }
+    if (myTag.indexOf('/script') == 0) {
+      return true;
+    }
+    if (myTag.indexOf('/style') == 0) {
+      return true;
+    }
+    return false;
   }
-  return false;
-}
 }
 
 img_thumb_width_reg = 180; // Image Thumbnail Width

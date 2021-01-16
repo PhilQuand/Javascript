1e6a46f198f55e70e8209ea75e49e3c5ddec033e
PhilQuand <philippe.quandalle@yahoo.fr>
2021-01-16 20:41:30 +0100

    Update corpsBRClass2Modal-nomodule.js

--- PhotoGaleries/myFancybox/corpsBRClass2Modal-nomodule.js
+++ PhotoGaleries/myFancybox/corpsBRClass2Modal-nomodule.js
@@ -1,5 +1,5 @@
 (function($) {
-  $.fn.Class2Modal = function(options) {
+  $.fn.corpsBRClass2Modal = function(options) {
     var _this = this;
     Promise.all([
       //  chargements des dépendances css
@@ -8,21 +8,40 @@
       load.js('https://philquand.github.io/Javascript/Hi-base64/hi-base64.js'),
     ]).then(
     function() {
-    var _options = { fancyClass: articleFancy, Num: "SCTLP" };
+    //var _options = { fancyClass: articleFancy, Num: "SCTLP" };
+    var _options = { Num: "SCTLP" };
+    if( jQuery.type(options.fancyClass) != 'undefined' ) _options.fancyClass = options.fancyClass;
+    if( jQuery.type(options.src) != 'undefined' ) _options.src = options.src;
     var href=[];
     var hrefURL = options.href;
     _options.href = href;
        for (var i = 0; i < hrefURL.length; i++) {
     var result = getMyInnerLinkContent(hrefURL[i]);
-    var corpsBR = result.corpsBR.wrap('<div class="corpsFancy corpsBR"></div>');
-    var title = result.title.html().split(",")[1]
+    //var corpsBR = result.corpsBR.wrap('<div class="corpsFancy corpsBR"></div>');
+    var corpsBR = result.corpsBR;
+    corpsBR.addClass('corpsFancy ');
+    //var title = result.title.html().split(",")[1];
+    if( jQuery.type(corpsBR.attr('data-title')) != 'undefined') {
+        var title = corpsBR.attr('data-title');
+        var date = title;
+    } else {
+        var title = result.title.html();
+        var date = title.split(",")[1];
+    }
     corpsBR = corpsBR.prepend('<div class="titreFancy">' + title + ' </div>');
-    var imgBase= getIMG(title.split(' '));
-    var hrefItem = { dataType : "inline", dataThumb: imgBase, dataSrc: corpsBR.html(), dataCaption: '<a href=&#34;' + hrefURL[i] + '&#34;>L&#39;article d&#39;origine</a>'};
+    var hrefItem = { dataType : "inline", dataSrc: corpsBR.html(), dataCaption: '<a href=&#34;' + hrefURL[i] + '&#34;>L&#39;article d&#39;origine</a>'};
+    if( jQuery.type(corpsBR.attr('data-thumb')) != 'undefined') {
+        hrefItem.dataThumb = corpsBR.attr('data-thumb');
+    } else {
+        var imgBase= getIMG(date.split(' '));
+        hrefItem.dataThumb = imgBase;
+    }
     href.push(hrefItem);
        }
+
        $(_this).AlaUne(_options);
-        function getIMG(svgInfos) {
+
+       function getIMG(svgInfos) {
             var svgTemp = $('<svg width="100%" height="100%" viewBox="0 0 400 400" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;"><rect x="10" y="10" width="100%" height="100%" stroke="blue" fill=white stroke-opacity="1.0" fill-opacity="0.8"/> <g transform="matrix(0.622762,0,0,0.695394,-73.9671,-168.06)"> <text x="187.961px" y="471px" style="' + "font-family:'ArialMT', 'Arial', sans-serif;font-size:96px;fill:rgb(244,60,61);" + '" id="l1"></text> <text x="310.68px" y="549.099px" style="' + "font-family:'ArialMT', 'Arial', sans-serif;font-size:64px;fill:rgb(244,60,61);" + '" id="l2"></text> <text x="319.57px" y="615.193px" style="' + "font-family:'ArialMT', 'Arial', sans-serif;font-size:64px;fill:rgb(244,60,61);" + '" id="l3"></text> <text x="372.945px" y="693.292px" style="' + "font-family:'ArialMT', 'Arial', sans-serif;font-size:64px;fill:rgb(244,60,61);" + '" id="l4"></text> </g></svg>');
           if (typeof svgInfos !== 'undefined') {
             for (var i = 0; i < svgInfos.length; i++) {
@@ -113,7 +132,7 @@
           switch (tag) {
             case 'script':
               element.async = true;
-              //element.type = 'module';
+              element.type = 'module';
               break;
             case 'link':
               element.type = 'text/css';

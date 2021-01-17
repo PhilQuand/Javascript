242df55e8364cbf1e7a9491e50483214d01d9fce
PhilQuand <philippe.quandalle@yahoo.fr>
2021-01-17 18:05:49 +0100

    Create Satirique-Archive-Brouillon.js

---
+++ Blog-widgets/Antivol/Satirique-Archive-Brouillon.js
@@ -0,0 +1,4 @@
+var archi = [
+  "https://brouillon-antivol.blogspot.com/2021/01/les-breves-du-satirique-janvier-2021.html",
+  "https://brouillon-antivol.blogspot.com/2020/12/les-breves-du-satirique-decembre-2020.html",
+]

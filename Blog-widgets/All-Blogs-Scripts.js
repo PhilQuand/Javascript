7f0b15263f374671d5f1b547b6f802a3d9e3d8c2
PhilQuand <philippe.quandalle@yahoo.fr>
2021-02-10 17:34:04 +0100

    Update All-Blogs-Scripts.js

--- Blog-widgets/All-Blogs-Scripts.js
+++ Blog-widgets/All-Blogs-Scripts.js
@@ -75,6 +75,16 @@ var loadVal = function(values, url) {
   return 'not found'
 }
 
+var isValidUrl = function validURL(str) {
+  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return !!pattern.test(str);
+}
+
 var deepEqual = function(x, y) {
   if (x === y) {
     return true;

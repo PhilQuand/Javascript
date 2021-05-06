f9ee0430f1a01c388f244f8cb4021527fd0bb2f8
PhilQuand <philippe.quandalle@yahoo.fr>
2021-05-06 14:00:55 +0200

    Update Satirique-Archive-Brouillon.js

--- Blog-widgets/Antivol/Satirique-Archive-Brouillon.js
+++ Blog-widgets/Antivol/Satirique-Archive-Brouillon.js
@@ -1,4 +1,5 @@
 var archi = [
+  "https://brouillon-antivol.blogspot.com/2021/05/les-breves-du-satirique-mai-2021.html",
   "https://brouillon-antivol.blogspot.com/2021/03/les-breves-du-satirique-avril-2021.html",
   "https://brouillon-antivol.blogspot.com/2021/01/les-breves-du-satirique-janvier-2021.html",
   "https://brouillon-antivol.blogspot.com/2020/12/les-breves-du-satirique-decembre-2020.html",

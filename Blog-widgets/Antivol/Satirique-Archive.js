0593ca2b4c672f09e3b9b6a67e6f483c066bdd1f
PhilQuand <philippe.quandalle@yahoo.fr>
2021-05-16 18:36:19 +0200

    Update Satirique-Archive.js

--- Blog-widgets/Antivol/Satirique-Archive.js
+++ Blog-widgets/Antivol/Satirique-Archive.js
@@ -1,4 +1,5 @@
 var archi = [
+  "https://www.lantivol.com/2021/05/les-breves-du-satirique-mai-2021.html",
   "https://www.lantivol.com/2021/04/les-breves-du-satirique-avril-2021.html",
   "https://www.lantivol.com/2021/02/les-breves-du-satirique-fevrier-2021.html",
   "https://www.lantivol.com/2020/12/les-breves-du-satirique-decembre-2020.html",

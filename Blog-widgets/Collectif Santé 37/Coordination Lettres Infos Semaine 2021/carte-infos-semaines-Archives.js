8d96869d263af98cf15d283026b605282982872e
PhilQuand <philippe.quandalle@yahoo.fr>
2021-06-07 17:42:53 +0200

    Update carte-infos-semaines-Archives.js

--- Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2021/carte-infos-semaines-Archives.js
+++ Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2021/carte-infos-semaines-Archives.js
@@ -1,12 +1,16 @@
     var archi = [
             {
+          svgInfos: ['Semaine 22', 'du 30 may', 'au 06 june', '2021'],
+          idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2021/Semaine 22.html',
+        },
+            {
           svgInfos: ['Semaine 21', 'du 24 may', 'au 30 may', '2021'],
           idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2021/Semaine 21.html',
+          idGlinkID: 'PreviousLetters'
         },
             {
           svgInfos: ['Semaine 20', 'du 17 may', 'au 23 may', '2021'],
           idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2021/Semaine 20.html',
-          idGlinkID: 'PreviousLetters'
         },
             {
           svgInfos: ['Semaine 19', 'du 10 may', 'au 16 may', '2021'],

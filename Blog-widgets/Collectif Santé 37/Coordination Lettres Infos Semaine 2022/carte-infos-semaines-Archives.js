d0b42b34f8f32bd0a50951801842bd3c8e48b788
PhilQuand <49251353+PhilQuand@users.noreply.github.com>
2022-01-17 11:14:04 +0100

    Update carte-infos-semaines-Archives.js

--- Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2022/carte-infos-semaines-Archives.js
+++ Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2022/carte-infos-semaines-Archives.js
@@ -1,12 +1,16 @@
     var archi = [
             {
+          svgInfos: ['Semaine 02', 'du 10 jan.', 'au 16 jan.', '2022'],
+          idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2022/Semaine 02.html',
+        },
+            {
           svgInfos: ['Semaine 01', 'du 03 jan.', 'au 09 jan.', '2022'],
           idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2022/Semaine 01.html',
+          idGlinkID: 'PreviousLetters'
         },
             {
           svgInfos: ['Semaines 52', 'du 27 dec.', 'au 02 jan.', '2021'],
           idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2021/Semaine 52.html',
-          idGlinkID: 'PreviousLetters'
         },
             {
           svgInfos: ['Semaines 51', 'du 20 dec.', 'au 26 dec.', '2021'],

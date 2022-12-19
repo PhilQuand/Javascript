e593af562e4afcf6e66c9aa9afac359a82bd5d1b
PhilQuand <49251353+PhilQuand@users.noreply.github.com>
2022-12-19 19:39:17 +0100

    carte CN

--- Blog-widgets/Collectif Santé 37/FermeturesHopitaux/Distribution v6/FermeturesHopitaux-v6.03.js
+++ Blog-widgets/Collectif Santé 37/FermeturesHopitaux/Distribution v6/FermeturesHopitaux-v6.03.js
@@ -25,7 +25,7 @@ $.fn.mapAllBlogs = function() {
       },
       carteMAJ: "19/12/2022",
       //INSEE_KEY: "INSEE_COM",
-      checkIfStoreNeedsUpdate: false,
+       checkIfStoreNeedsUpdate: false,
       dataTransformer: false,
       /*dataTransformer: function(inputData, setCallBack) {
       var outputData = 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/FermeturesHopitaux/Applications/AnalyseCN/export_Tous_les_établissements_concernés-2021_02_23.js';

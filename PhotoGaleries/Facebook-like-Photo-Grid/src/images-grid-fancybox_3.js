67ffb9e92e038429c7cb46dd6aca899f8b653573
PhilQuand <49251353+PhilQuand@users.noreply.github.com>
2023-04-10 12:35:05 +0200

    Update images-grid-fancybox_3.js

--- PhotoGaleries/Facebook-like-Photo-Grid/src/images-grid-fancybox_3.js
+++ PhotoGaleries/Facebook-like-Photo-Grid/src/images-grid-fancybox_3.js
@@ -1,23 +1,14 @@
 (function($) {
-
-  /**
-   * Plugin
-   */
-
   $.fn.imagesGrid = function(options) {
-
     var args = arguments;
-
     return this.each(function() {
 
       var _this = this;
       Promise.all([
         //  chargements des dépendances css
-        load.css('https://philquand.github.io/Javascript/PhotoGaleries/Facebook-like-Photo-Grid/src/images-grid.css'),
-        load.css('https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.css'),
+        load.css('https://philquand.github.io/Javascript/PhotoGaleries/Facebook-like-Photo-Grid/src/images-grid_3.css'),
         load.css('https://philquand.github.io/Javascript/PhotoGaleries/myFancybox/myFancybox.css'),
         //  chargements des dépendances js
-        load.js('https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.js'),
         load.js('https://philquand.github.io/Javascript/PhotoGaleries/myFancybox/myFancybox.js'),
       ]).then(function() {
         console.log('Everything has loaded!');

8176febd3d3174177ddd50d75457f279d1f149c6
PhilQuand <49251353+PhilQuand@users.noreply.github.com>
2023-04-13 14:19:21 +0200

    Update images-grid-fancybox.js

--- PhotoGaleries/Facebook-like-Photo-Grid/src/images-grid-fancybox.js
+++ PhotoGaleries/Facebook-like-Photo-Grid/src/images-grid-fancybox.js
@@ -2,7 +2,6 @@
 Promise.all([
   //  chargements des dépendances css
   load.css('https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css'),
-  load.css('https://philquand.github.io/Javascript/PhotoGaleries/myFancybox/AlaUneFB4.css'),
   //  chargements des dépendances js
   load.js('https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js'),
 ]).then(

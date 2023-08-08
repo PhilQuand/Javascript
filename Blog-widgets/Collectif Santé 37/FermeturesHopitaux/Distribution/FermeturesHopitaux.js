3cab91d14bf0e5917175818cb90471ad7e42b413
PhilQuand <49251353+PhilQuand@users.noreply.github.com>
2023-08-08 08:34:29 +0200

    Update FermeturesHopitaux.js

--- Blog-widgets/Collectif Santé 37/FermeturesHopitaux/Distribution/FermeturesHopitaux.js
+++ Blog-widgets/Collectif Santé 37/FermeturesHopitaux/Distribution/FermeturesHopitaux.js
@@ -23,7 +23,7 @@ $.fn.mapAllBlogs = function(optUdate) {
         if (!isNaN(iData[iVar])) lieux += ', France';*/
         return lieux;
       },
-      carteMAJ: "31/07/2023",
+      carteMAJ: "08/08/2023",
       //tableAccess: false,
       //INSEE_KEY: "INSEE_COM",
       //checkIfStoreNeedsUpdate: true,

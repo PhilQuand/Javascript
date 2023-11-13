b05677035e4320f33b95bab66fd56fceec7ae438
PhilQuand <49251353+PhilQuand@users.noreply.github.com>
2023-11-13 14:21:15 +0100

    Update carte-infos-semaines-Archives.js

--- Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2023/carte-infos-semaines-Archives.js
+++ Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2023/carte-infos-semaines-Archives.js
@@ -1,12 +1,16 @@
     var archi = [
               {
+          svgInfos: ['Semaine 45', 'du 06 nov.', 'au 12 nov.', '2023'],
+          idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2023/Semaine 44.html',
+        }, 
+              {
           svgInfos: ['Semaine 44', 'du 30 oct.', 'au 05 nov.', '2023'],
           idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2023/Semaine 44.html',
+          idGlinkID: 'PreviousLetters'
         }, 
               {
           svgInfos: ['Semaine 43', 'du 23 oct.', 'au 29 oct.', '2023'],
           idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2023/Semaine 43.html',
-          idGlinkID: 'PreviousLetters'
         }, 
               {
           svgInfos: ['Semaine 42', 'du 16 oct.', 'au 22 oct.', '2023'],

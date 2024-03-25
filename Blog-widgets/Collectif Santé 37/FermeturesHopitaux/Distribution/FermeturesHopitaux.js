7f78a9aadd6521c6fc26419b965d3dee2b40e7f7
PhilQuand <49251353+PhilQuand@users.noreply.github.com>
2024-03-25 09:21:23 +0100

    Cartes Hopitaux

--- Blog-widgets/Collectif Santé 37/FermeturesHopitaux/Distribution/FermeturesHopitaux.js
+++ Blog-widgets/Collectif Santé 37/FermeturesHopitaux/Distribution/FermeturesHopitaux.js
@@ -23,7 +23,7 @@ $.fn.mapAllBlogs = function(optUdate) {
         if (!isNaN(iData[iVar])) lieux += ', France';*/
         return lieux;
       },
-      carteMAJ: "11/03/2024",
+      carteMAJ: "25/03/2024",
       //tableAccess: false,
       //INSEE_KEY: "INSEE_COM",
       //checkIfStoreNeedsUpdate: true,

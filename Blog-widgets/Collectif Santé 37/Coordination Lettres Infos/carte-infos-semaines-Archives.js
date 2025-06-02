6d0b019b5fbca73d7f42a0a0387cedec44d60a04
PhilQuand <49251353+PhilQuand@users.noreply.github.com>
2025-06-02 15:57:18 +0200

    Update carte-infos-semaines-Archives.js

--- Blog-widgets/Collectif Santé 37/Coordination Lettres Infos/carte-infos-semaines-Archives.js
+++ Blog-widgets/Collectif Santé 37/Coordination Lettres Infos/carte-infos-semaines-Archives.js
@@ -1,12 +1,16 @@
     var archi = [
                {
+          svgInfos: ['Semaine 22', 'du 26 mai', 'au 1er juin', '2025'],
+          idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2025/Semaine 22.html',
+        }, 
+               {
           svgInfos: ['Semaine 21', 'du 20 mai', 'au 25 mai.', '2025'],
           idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2025/Semaine 21.html',
+          idGlinkID: 'PreviousLetters'
         }, 
                {
           svgInfos: ['Semaine 20', 'du 12 mai', 'au 19 mai.', '2025'],
           idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2025/Semaine 20.html',
-          idGlinkID: 'PreviousLetters'
         }, 
                {
           svgInfos: ['Semaine 19', 'du 05 mai', 'au 11 mai.', '2025'],

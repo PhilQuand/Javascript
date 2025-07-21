a36abc678003db9a3b98922cc27a300065de03de
PhilQuand <49251353+PhilQuand@users.noreply.github.com>
2025-07-21 09:56:35 +0200

    Update carte-infos-semaines-Archives.js

--- Blog-widgets/Collectif Santé 37/Coordination Lettres Infos/carte-infos-semaines-Archives.js
+++ Blog-widgets/Collectif Santé 37/Coordination Lettres Infos/carte-infos-semaines-Archives.js
@@ -1,5 +1,5 @@
     var archi = [
-               { 
+               {
           svgInfos: ['Semaine 29', 'du 14 juil.', 'au 20 juil.', '2025'],
           idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2025/Semaine 29.html',
         }, 

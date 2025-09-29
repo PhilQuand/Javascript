0961e30bd3f1cbdaaca1703870cf758a83d0ab82
PhilQuand <49251353+PhilQuand@users.noreply.github.com>
2025-09-29 12:42:32 +0200

    Update carte-infos-semaines-Archives.js

--- Blog-widgets/Collectif Santé 37/Coordination Lettres Infos/carte-infos-semaines-Archives.js
+++ Blog-widgets/Collectif Santé 37/Coordination Lettres Infos/carte-infos-semaines-Archives.js
@@ -1,12 +1,16 @@
     var archi = [
                {
+          svgInfos: ['Semaine 39', 'du 22 sept.', 'au 28 sept.', '2025'],
+          idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2025/Semaine 39.html',
+        }, 
+               {
           svgInfos: ['Semaine 38', 'du 15 sept.', 'au 21 sept.', '2025'],
           idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2025/Semaine 38.html',
+          idGlinkID: 'PreviousLetters'
         }, 
                {
           svgInfos: ['Semaine 37', 'du 08 sept.', 'au 14 sept.', '2025'],
           idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2025/Semaine 37.html',
-          idGlinkID: 'PreviousLetters'
         }, 
                {
           svgInfos: ['Semaine 36', 'du 01 sept.', 'au 07 sept.', '2025'],

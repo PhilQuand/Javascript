b34bf5e7aacc3109b49543371cc73d806be2c601
PhilQuand <49251353+PhilQuand@users.noreply.github.com>
2026-04-13 18:41:02 +0200

    Update carte-infos-semaines-Archives.js

--- Blog-widgets/Collectif Santé 37/Coordination Lettres Infos/carte-infos-semaines-Archives.js
+++ Blog-widgets/Collectif Santé 37/Coordination Lettres Infos/carte-infos-semaines-Archives.js
@@ -1,12 +1,16 @@
     var archi = [
                  {
+          svgInfos: ['Semaine 15', 'du 06 avr.', 'au 12 avril', '2026'],
+          idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2026/Semaine 15.html',
+        }, 
+                 {
           svgInfos: ['Semaine 14', 'du 30 mars', 'au 06 avril', '2026'],
           idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2026/Semaine 14.html',
+          idGlinkID: 'PreviousLetters'
         }, 
                  {
           svgInfos: ['Semaine 13', 'du 23 mars', 'au 29 mars', '2026'],
           idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2026/Semaine 13.html',
-          idGlinkID: 'PreviousLetters'
         }, 
                  {
           svgInfos: ['Semaine 12', 'du 16 mars', 'au 22 mars', '2026'],

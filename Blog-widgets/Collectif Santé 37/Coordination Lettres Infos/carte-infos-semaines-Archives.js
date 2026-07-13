f20589830a3b041eb38eb4af559a6c649c2f64f8
PhilQuand <49251353+PhilQuand@users.noreply.github.com>
2026-07-13 11:56:25 +0200

    Update carte-infos-semaines-Archives.js

--- Blog-widgets/Collectif Santé 37/Coordination Lettres Infos/carte-infos-semaines-Archives.js
+++ Blog-widgets/Collectif Santé 37/Coordination Lettres Infos/carte-infos-semaines-Archives.js
@@ -1,11 +1,15 @@
     var archi = [
                   {
-          svgInfos: ['Semaine 27', 'du 29 juin', 'au 05 juillet', '2026'],
-          idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2026/Semaine 26.html',
+          svgInfos: ['Semaine 28', 'du 06 juil.', 'au 12 juil.', '2026'],
+          idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2026/Semaine 28.html',
+        }, 
+                  {
+          svgInfos: ['Semaine 27', 'du 29 juin', 'au 05 juil.', '2026'],
+          idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2026/Semaine 27.html',
+          idGlinkID: 'PreviousLetters'
         }, 
           svgInfos: ['Semaine 26', 'du 22 juin', 'au 28 juin', '2026'],
           idG: 'https://philquand.github.io/Javascript/Blog-widgets/Collectif Santé 37/Coordination Lettres Infos Semaine 2026/Semaine 26.html',
-          idGlinkID: 'PreviousLetters'
         }, 
                   {
           svgInfos: ['Semaine 25', 'du 15 juin', 'au 21 juin', '2026'],
